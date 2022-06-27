511ba10f2c1cb4535b679c4f1cbeb18e03fa3899
ryanwolhuter <dev@ryanwolhuter.com>
2022-06-27 12:04:52 +0200

    add and configure react-query
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "components/GlobalStyle";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
