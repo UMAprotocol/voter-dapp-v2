08c1badd149d17cbf90599f753ae13d6abe04ddb
ryanwolhuter <dev@ryanwolhuter.com>
2022-06-29 11:33:50 +0200

    configure blocknative app metadata
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- helpers/initOnboard.ts
+++ helpers/initOnboard.ts
@@ -2,10 +2,14 @@ import { init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
 import walletConnectModule from "@web3-onboard/walletconnect";
 import gnosisModule from "@web3-onboard/gnosis";
+import { logo } from "public/assets/logo";
 
 const injected = injectedModule();
 const walletConnect = walletConnectModule();
 const gnosis = gnosisModule();
+
+const blocknativeDappId = process.env.NEXT_PUBLIC_BLOCKNATIVE_DAPP_ID;
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
 
 export const initOnboard = init({
   wallets: [injected, walletConnect, gnosis],
@@ -14,7 +18,15 @@ export const initOnboard = init({
       id: "0x1",
       token: "ETH",
       label: "Ethereum Mainnet",
-      rpcUrl: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
+      rpcUrl: `https://mainnet.infura.io/v3/${infuraId}`,
     },
   ],
+  appMetadata: {
+    name: "UMA 2.0",
+    icon: logo,
+    logo,
+    description: "UMA 2.0 Voter Dapp",
+    recommendedInjectedWallets: [{ name: "MetaMask", url: "https://metamask.io" }],
+  },
+  apiKey: blocknativeDappId,
 });
