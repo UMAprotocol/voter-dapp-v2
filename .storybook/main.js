676f5d774e8f621878d16fed2bfd8bf6a0a4610a
ryanwolhuter <dev@ryanwolhuter.com>
2022-06-30 09:44:26 +0200

    remove redundant ts config paths webpack plugin
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -14,14 +12,5 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
-  },
-  webpackFinal: async (config) => {
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin({
-        extensions: config.resolve.extensions,
-      }),
-    ];
-    return config;
-  },
+  }
 }
