6f86c96554df6fc094fa03aeef80e03aebcfa71e
ryanwolhuter <dev@ryanwolhuter.com>
2022-06-30 09:43:36 +0200

    add nextjs storybook addon
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-addon-next"
   ],
   "framework": "@storybook/react",
   "core": {
