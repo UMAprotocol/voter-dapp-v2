1a950da223c8c108d5e826ae765209a08203605c
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-04 12:08:18 +0200

    wire up storybook args to component state
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/Dropdown.stories.tsx
+++ stories/Dropdown.stories.tsx
@@ -1,24 +1,35 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Dropdown } from "components/Dropdown";
+import { useArgs } from "@storybook/client-api";
 
-export default {
-  title: "Dropdown",
-  component: Dropdown,
-} as ComponentMeta<typeof Dropdown>;
-
-const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
-
-export const Default = Template.bind({});
 const defaultMockItems = [
   { label: "Item 1", value: "item-1", secondaryLabel: "Item 1 secondary label" },
   { label: "Item 2", value: "item-2", secondaryLabel: "Item 2 secondary label" },
   { label: "Item 3", value: "item-3", secondaryLabel: "Item 3 secondary label" },
   { label: "Item 4", value: "item-4", secondaryLabel: "Item 4 secondary label" },
 ];
-Default.args = {
-  items: defaultMockItems,
-  selected: defaultMockItems[0],
-  onSelect: (item) => {
-    console.log(`Selected ${item.label}`);
+
+export default {
+  title: "Dropdown",
+  component: Dropdown,
+  args: {
+    label: "Dropdown label",
+    selected: null,
+    items: defaultMockItems,
   },
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof Dropdown> = (args) => {
+  const [_args, updateArgs] = useArgs();
+
+  return (
+    <Dropdown
+      {...args}
+      onSelect={(item) => {
+        updateArgs({ selected: item });
+      }}
+    />
+  );
 };
+
+export const Default = Template.bind({});
