215a9ecbb7fb4ab9df4afec4048e4a6d8a6c846a
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-06 14:17:00 +0200

    add primary button variant
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,13 @@
+import { red, white } from "constants/colors";
 import Link from "next/link";
 import { ReactNode } from "react";
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
 
 interface Props {
+  /**
+   * use `primary` for the most important UI element on the page
+   */
+  variant?: "primary" | "secondary";
   /**
    * button contents
    */
@@ -20,20 +25,53 @@ interface Props {
 /**
  * Primary UI component for user interaction
  *
+ * @param variant use `primary` for the most important UI element on the page
  * @param onClick - behaves as a button when provided
  * @param href - behaves as a link when provided
  * @param label - button label
  * @throws if both onClick and href are provided
  */
-export function Button({ label, onClick, href }: Props) {
+export function Button({ variant = "secondary", label, onClick, href }: Props) {
   if (onClick && href) {
     throw new Error("Cannot have both onClick and href. Must behave as either a link or a button.");
   }
 
+  const primaryStyle = {
+    "--display": "grid",
+    "--place-items": "center",
+    "--color": white,
+    "--background-color": red,
+    "--width": 200 + "px",
+    "--height": 50 + "px",
+    "--border-radius": 5 + "px",
+    "--font-size": 18 + "px",
+  } as CSSProperties;
+
+  const secondaryStyle = {
+    "--display": "unset",
+    "--place-items": "unset",
+    "--color": red,
+    "--background-color": "transparent",
+    "--width": "unset",
+    "--height": "unset",
+    "--border-radius": "unset",
+    "--font-size": 16 + "px",
+  } as CSSProperties;
+
+  const style = variant === "primary" ? primaryStyle : secondaryStyle;
+
   return (
     <>
-      {href ? <_Link href={href}>{label}</_Link> : null}
-      {onClick ? <_Button onClick={onClick}>{label}</_Button> : null}
+      {href ? (
+        <_Link href={href} style={style}>
+          {label}
+        </_Link>
+      ) : null}
+      {onClick ? (
+        <_Button onClick={onClick} style={style}>
+          {label}
+        </_Button>
+      ) : null}
     </>
   );
 }
@@ -41,31 +79,50 @@ export function Button({ label, onClick, href }: Props) {
 interface LinkProps {
   href: string;
   children: ReactNode;
+  style: CSSProperties;
 }
-function _Link({ href, children }: LinkProps) {
+function _Link({ href, children, style }: LinkProps) {
   return (
     <Link href={href} passHref>
-      <A>{children}</A>
+      <A style={style}>{children}</A>
     </Link>
   );
 }
 
 const A = styled.a`
+  display: var(--display);
+  place-items: var(--place-items);
+  width: var(--width);
+  height: var(--height);
   text-decoration: none;
-  color: var(--red);
+  color: var(--color);
+  background-color: var(--background-color);
+  border-radius: var(--border-radius);
   font: var(--text-md);
+  font-size: var(--font-size);
 `;
 
 interface ButtonProps {
   onClick: () => void;
   children: ReactNode;
+  style: CSSProperties;
 }
-function _Button({ onClick, children }: ButtonProps) {
-  return <__Button onClick={onClick}>{children}</__Button>;
+function _Button({ onClick, children, style }: ButtonProps) {
+  return (
+    <__Button onClick={onClick} style={style}>
+      {children}
+    </__Button>
+  );
 }
 
 const __Button = styled.button`
-  color: var(--red);
-  background: transparent;
+  display: var(--display);
+  place-items: var(--place-items);
+  width: var(--width);
+  height: var(--height);
+  color: var(--color);
+  background-color: var(--background-color);
+  border-radius: var(--border-radius);
   font: var(--text-md);
+  font-size: var(--font-size);
 `;
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -15,9 +15,21 @@ AsButton.args = {
   href: undefined,
 };
 
+export const AsButtonPrimary = Template.bind({});
+AsButtonPrimary.args = {
+  ...AsButton.args,
+  variant: "primary",
+};
+
 export const AsLink = Template.bind({});
 AsLink.args = {
   label: "Link",
   href: "https://www.google.com",
   onClick: undefined,
+};
+
+export const AsLinkPrimary = Template.bind({});
+AsLinkPrimary.args = {
+  ...AsLink.args,
+  variant: "primary",
 };
