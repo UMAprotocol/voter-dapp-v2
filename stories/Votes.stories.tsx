76174abf6e5a75aa8ee47238148551f476bbd26f
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-06 13:52:08 +0200

    add votes story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ stories/Votes.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Votes } from "components/Votes";
+import { VoteTimelineT } from "types/global";
+import * as VoteBarStories from "./VoteBar.stories";
+import { CommitPhase } from "./VoteTimeline/VoteTimeline.stories";
+
+export default {
+  title: "Votes",
+  component: Votes,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 1190 }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Votes>;
+
+const Template: ComponentStory<typeof Votes> = (args) => <Votes {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  votes: [
+    VoteBarStories.OriginUmaNotCommitted.args!.vote!,
+    VoteBarStories.OriginPolymarketNotCommitted.args!.vote!,
+    VoteBarStories.OriginUmaCommitted.args!.vote!,
+    VoteBarStories.OriginPolymarketCommitted.args!.vote!,
+  ],
+  voteTimeline: CommitPhase.args! as VoteTimelineT,
+  moreDetailsAction: () => console.log("More details clicked"),
+};
