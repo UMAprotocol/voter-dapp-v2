8e923a7aee22739e269c060de82cc54323e7c93c
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-06 10:36:29 +0200

    add vote bar styles
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/VoteBar/VoteBar.tsx
+++ components/VoteBar/VoteBar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "components/Button";
 import { Dropdown } from "components/Dropdown";
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
 import { DisputeOriginT, DisputeT, DropdownItemT } from "types/global";
 import UMA from "public/assets/icons/uma.svg";
 import Polymarket from "public/assets/icons/polymarket.svg";
+import Dot from "public/assets/icons/dot.svg";
+import { green, red } from "constants/colors";
 
 interface Props {
   dispute: DisputeT;
@@ -16,6 +18,8 @@ export function VoteBar({ dispute, voteOptions, isCommitted, moreDetailsAction }
   const [vote, setVote] = useState<DropdownItemT | null>(null);
 
   const Icon = dispute.origin === DisputeOriginT.UMA ? UMAIcon : PolymarketIcon;
+
+  const dotColor = isCommitted ? green : red;
 
   return (
     <Wrapper>
@@ -31,7 +35,16 @@ export function VoteBar({ dispute, voteOptions, isCommitted, moreDetailsAction }
       <Vote>
         <Dropdown label="Choose answer" items={voteOptions} selected={vote} onSelect={(vote) => setVote(vote)} />
       </Vote>
-      <Status>{isCommitted ? "Committed" : "Not committed"}</Status>
+      <Status>
+        <DotIcon
+          style={
+            {
+              "--dot-color": dotColor,
+            } as CSSProperties
+          }
+        />{" "}
+        {isCommitted ? "Committed" : "Not committed"}
+      </Status>
       <MoreDetails>
         <Button label="More details" onClick={moreDetailsAction} />
       </MoreDetails>
@@ -49,24 +62,47 @@ const Wrapper = styled.div`
 
 const Dispute = styled.div`
   justify-self: start;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-left: 30px;
 `;
 
 const DisputeDetailsWrapper = styled.div``;
 
 const DisputeIconWrapper = styled.div``;
 
-const DisputeTitle = styled.h3``;
+const DisputeTitle = styled.h3`
+  font: var(--header-sm);
+`;
 
-const DisputeOrigin = styled.h4``;
+const DisputeOrigin = styled.h4`
+  font: var(--text-xs);
+  color: var(--black-opacity-50);
+`;
 
 const Vote = styled.div`
   width: 240px;
 `;
 
-const Status = styled.div``;
+const Status = styled.div`
+  width: 144px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font: var(--text-md);
+`;
 
-const MoreDetails = styled.div``;
+const MoreDetails = styled.div`
+  justify-self: end;
+`;
 
 const UMAIcon = styled(UMA)``;
 
 const PolymarketIcon = styled(Polymarket)``;
+
+const DotIcon = styled(Dot)`
+  circle {
+    fill: var(--dot-color);
+  }
+`;
