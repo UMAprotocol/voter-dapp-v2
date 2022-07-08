b641f5c70e7f803f57a4aca7e1a02acb9b474012
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-08 10:44:57 +0200

    add panel context to storybook preview
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import { addDecorator } from "@storybook/react";
 import { GlobalStyle } from 'components/GlobalStyle';
 import { WalletProvider } from "contexts/WalletContext";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { PanelProvider } from "contexts/PanelContext";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -12,9 +14,13 @@ export const parameters = {
   },
 }
 
-addDecorator((story) => (
+addDecorator((Story) => (
   <WalletProvider>
-    <GlobalStyle />
-    {story()}
+    <QueryClientProvider client={new QueryClient()}>
+      <PanelProvider>
+        <GlobalStyle />
+        <Story />
+      </PanelProvider>
+    </QueryClientProvider>
   </WalletProvider>
 ));
