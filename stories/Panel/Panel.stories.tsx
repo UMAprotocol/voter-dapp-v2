d0bb6f8a7514c9df9812d530936d210d140ffa2e
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-08 11:10:23 +0200

    use args to show hide panel in panel story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/Panel/Panel.stories.tsx
+++ stories/Panel/Panel.stories.tsx
@@ -1,23 +1,32 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Panel } from "components/Panel";
+import { useArgs } from "@storybook/client-api";
+import { Button } from "components/Button";
 
 export default {
   title: "Panel",
   component: Panel,
 } as ComponentMeta<typeof Panel>;
 
-const Template: ComponentStory<typeof Panel> = (args) => <Panel {...args} />;
+const Template: ComponentStory<typeof Panel> = (args) => {
+  const [_args, updateArgs] = useArgs();
+
+  return (
+    <div>
+      <Button variant="primary" onClick={() => updateArgs({ isOpen: true })} label="Open panel" />
+      <Panel {...args} onDismiss={() => updateArgs({ isOpen: false })} />
+    </div>
+  );
+};
 
 export const AsClaimPanel = Template.bind({});
 AsClaimPanel.args = {
   panelType: "claim",
   isOpen: true,
-  onDismiss: () => {},
 };
 
 export const AsVotePanel = Template.bind({});
 AsVotePanel.args = {
   panelType: "vote",
   isOpen: true,
-  onDismiss: () => {},
 };
