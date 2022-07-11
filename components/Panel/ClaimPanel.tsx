9598983b057882809a724fcfc14ce44792b55488
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-11 12:45:29 +0200

    add claim panel markup
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Panel/ClaimPanel.tsx
+++ components/Panel/ClaimPanel.tsx
@@ -1,8 +1,54 @@
+import { Button } from "components/Button";
+import styled from "styled-components";
 import { PanelContentT } from "types/global";
 
 interface Props {
   content: PanelContentT;
 }
 export function ClaimPanel({ content }: Props) {
-  return <div>Claim Panel</div>;
+  return (
+    <Wrapper>
+      <RewardsWrapper>
+        <RewardsHeader>Claimable Rewards</RewardsHeader>
+        <Rewards>
+          <strong>92.678</strong> UMA
+        </Rewards>
+      </RewardsWrapper>
+      <ClaimAndStakeWrapper>
+        <ClaimAndStakeHeader>Claim and Stake</ClaimAndStakeHeader>
+        <ClaimAndStakeDescription>
+          Earn even more rewards by claiming and automatically stake/lock these rewards text TODO
+        </ClaimAndStakeDescription>
+        <Button variant="primary" label="Claim and Stake" onClick={() => console.log("TODO Claim and Stake")} />
+      </ClaimAndStakeWrapper>
+      <ClaimToWalletWrapper>
+        <ClaimToWalletHeader>Claim to Wallet</ClaimToWalletHeader>
+        <ClaimToWalletDescription>
+          By claiming to your wallet you will not earn rewards text text but this could be an option for tax reasons
+          text TODO.
+        </ClaimToWalletDescription>
+        <Button variant="secondary" label="Claim to Wallet" onClick={() => console.log("TODO Claim to Wallet")} />
+      </ClaimToWalletWrapper>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
+
+const RewardsWrapper = styled.div``;
+
+const RewardsHeader = styled.h2``;
+
+const Rewards = styled.p``;
+
+const ClaimAndStakeWrapper = styled.div``;
+
+const ClaimAndStakeHeader = styled.h2``;
+
+const ClaimAndStakeDescription = styled.p``;
+
+const ClaimToWalletWrapper = styled.div``;
+
+const ClaimToWalletHeader = styled.h2``;
+
+const ClaimToWalletDescription = styled.p``;
