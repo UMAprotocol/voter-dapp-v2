ab62a1d605e1b719b78d7a8845e762cee6bf22c1
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-11 15:40:59 +0200

    add tabs component and story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ components/Tabs/Tabs.tsx
@@ -0,0 +1,61 @@
+import {
+  Tab as ReachTab,
+  TabList as ReachTabList,
+  TabPanel as ReachTabPanel,
+  TabPanels as ReachTabPanels,
+  Tabs as ReachTabs,
+} from "@reach/tabs";
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+type Tab = {
+  title: string;
+  content: ReactNode;
+};
+
+interface Props {
+  tabs: Tab[];
+}
+export function Tabs({ tabs }: Props) {
+  return (
+    <TabsWrapper>
+      <TabList>
+        {tabs.map(({ title }) => (
+          <Tab key={title}>{title}</Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {tabs.map(({ content }) => (
+          <TabPanel key={content?.toString()}>{content}</TabPanel>
+        ))}
+      </TabPanels>
+    </TabsWrapper>
+  );
+}
+const TabsWrapper = styled(ReachTabs)``;
+
+const TabList = styled(ReachTabList)`
+  width: 100%;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  gap: 50px;
+  padding-left: 30px;
+  background: var(--gray-50);
+
+  [data-selected] {
+    border-bottom: 3px solid var(--red);
+  }
+`;
+
+const Tab = styled(ReachTab)`
+  height: 100%;
+  background: transparent;
+  color: var(--black);
+  font: var(--text-md);
+  border-bottom: 3px solid transparent;
+`;
+
+const TabPanels = styled(ReachTabPanels)``;
+
+const TabPanel = styled(ReachTabPanel)``;
---
+++ stories/Tabs.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Tabs } from "components/Tabs";
+
+export default {
+  title: "Tabs",
+  component: Tabs,
+} as ComponentMeta<typeof Tabs>;
+
+const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
+
+const tabs = [
+  {
+    title: "Stake",
+    content: <div>Stake panel</div>,
+  },
+  {
+    title: "Unstake",
+    content: <div>Unstake panel</div>,
+  },
+];
+
+export const Default = Template.bind({});
+Default.args = {
+  tabs,
+};
