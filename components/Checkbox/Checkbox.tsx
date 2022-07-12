6bb0be14bd6916afbde88465d39eb95c8cac8837
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-12 10:38:54 +0200

    add custom checkbox library
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -1,3 +1,6 @@
+import { CustomCheckbox, CustomCheckboxContainer, CustomCheckboxInput } from "@reach/checkbox";
+import "@reach/checkbox/styles.css";
+
 export function Checkbox() {
   return <div></div>;
 }
