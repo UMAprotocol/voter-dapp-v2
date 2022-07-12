f71e7a3f80c65d5ffe3592c66bdbc925527af36e
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-12 13:48:36 +0200

    add checkbox styles
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -1,6 +1,54 @@
-import { CustomCheckbox, CustomCheckboxContainer, CustomCheckboxInput } from "@reach/checkbox";
+import { CustomCheckboxContainer, CustomCheckboxInput } from "@reach/checkbox";
 import "@reach/checkbox/styles.css";
+import { black } from "constants/colors";
+import { ChangeEvent, ReactNode } from "react";
+import styled, { CSSProperties } from "styled-components";
+import Check from "public/assets/icons/check.svg";
 
-export function Checkbox() {
-  return <div></div>;
+interface Props {
+  label: ReactNode;
+  checked: boolean;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
 }
+export function Checkbox({ label, checked, onChange, disabled }: Props) {
+  const boxBackgroundColor = checked ? black : "transparent";
+  return (
+    <Label>
+      <Box
+        checked={checked}
+        disabled={disabled}
+        onChange={onChange}
+        style={
+          {
+            "--box-background-color": boxBackgroundColor,
+          } as CSSProperties
+        }
+      >
+        <CustomCheckboxInput />
+        {checked && <CheckIcon />}
+      </Box>
+      {label}
+    </Label>
+  );
+}
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  color: var(--black);
+  font: var(--text-xs);
+`;
+
+const Box = styled(CustomCheckboxContainer)`
+  width: 16px;
+  height: 16px;
+  display: inline-grid;
+  place-items: center;
+  background: var(--box-background-color);
+  border: 1px solid var(--black);
+  border-radius: 2px;
+`;
+
+const CheckIcon = styled(Check)``;
--- stories/Checkbox.stories.tsx
+++ stories/Checkbox.stories.tsx
@@ -1,9 +1,32 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Checkbox } from "components/Checkbox";
+import { useArgs } from "@storybook/client-api";
 
 export default {
   title: "Checkbox",
   component: Checkbox,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 500 }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof Checkbox>;
 
-const Template: ComponentStory<typeof Checkbox> = () => <Checkbox />;
+const Template: ComponentStory<typeof Checkbox> = (args) => {
+  const [_args, updateArgs] = useArgs();
+
+  return <Checkbox {...args} onChange={(e) => updateArgs({ checked: e.target.checked })} />;
+};
+
+export const Unchecked = Template.bind({});
+Unchecked.args = {
+  checked: false,
+  label: "Unchecked",
+};
+export const Checked = Template.bind({});
+Checked.args = {
+  checked: true,
+  label: "Checked",
+};
