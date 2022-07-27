8740cce2a687df8e168d9a7cf247f1b694084e8c
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-27 12:52:54 +0200

    add toggle component story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ stories/Toggle.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Toggle } from "components/Toggle";
+import { useArgs } from "@storybook/client-api";
+
+export default {
+  title: "Toggle",
+  component: Toggle,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 240 }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Toggle>;
+
+const Template: ComponentStory<typeof Toggle> = (args) => {
+  const [_args, updateArgs] = useArgs();
+
+  return <Toggle {...args} onClick={() => updateArgs({ clicked: !args.clicked })} />;
+};
+
+export const NotClicked = Template.bind({});
+NotClicked.args = {
+  clicked: false,
+};
+
+export const Clicked = Template.bind({});
+Clicked.args = {
+  clicked: true,
+};
