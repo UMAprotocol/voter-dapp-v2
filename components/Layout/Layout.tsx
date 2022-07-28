ab2c59ce5a979fba8c5753ed0881b009a5916b7a
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-28 16:20:41 +0200

    remove panel show hide logic from layout
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -7,18 +7,11 @@ interface Props {
   children: ReactNode;
 }
 export function Layout({ children }: Props) {
-  const { panelType, panelContent, panelOpen, setPanelOpen } = usePanelContext();
-
   return (
     <>
       <Header />
       <main>{children}</main>
-      <Panel
-        isOpen={panelOpen}
-        panelType={panelType}
-        panelContent={panelContent}
-        onDismiss={() => setPanelOpen(false)}
-      />
+      <Panel />
     </>
   );
 }
