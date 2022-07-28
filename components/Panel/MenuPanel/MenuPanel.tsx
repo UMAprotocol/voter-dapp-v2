f1a9bb4ae47c3524b9d9aa1cd190bf6df1a8e4a9
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-28 11:25:00 +0200

    add nav to menu panel
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Panel/MenuPanel/MenuPanel.tsx
+++ components/Panel/MenuPanel/MenuPanel.tsx
@@ -1,10 +1,33 @@
 import { Nav } from "components/Nav";
 import styled from "styled-components";
 
+const links = [
+  {
+    title: "Vote",
+    href: "/",
+  },
+  {
+    title: "Settled Disputes & Votes",
+    href: "/settled",
+  },
+  {
+    title: "Two Key Voting",
+    href: "/two-key",
+  },
+  {
+    title: "Optimistic Oracle",
+    href: "https://oracle.umaproject.org",
+  },
+  {
+    title: "Docs",
+    href: "https://docs.umaproject.org",
+  },
+];
+
 export function MenuPanel() {
   return (
     <Wrapper>
-      <Nav></Nav>
+      <Nav links={links} />
     </Wrapper>
   );
 }
