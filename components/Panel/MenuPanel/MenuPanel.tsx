661ec6ed933980e1d025caee1982e09ae315a202
ryanwolhuter <dev@ryanwolhuter.com>
2022-07-28 13:34:38 +0200

    add menu panel footer markup
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Panel/MenuPanel/MenuPanel.tsx
+++ components/Panel/MenuPanel/MenuPanel.tsx
@@ -1,5 +1,12 @@
 import { Nav } from "components/Nav";
 import styled from "styled-components";
+import Discord from "public/assets/icons/discord.svg";
+import Discourse from "public/assets/icons/discourse.svg";
+import Twitter from "public/assets/icons/twitter.svg";
+import Github from "public/assets/icons/github.svg";
+import Message from "public/assets/icons/message.svg";
+import Uma from "public/assets/icons/uma.svg";
+import Link from "next/link";
 
 const links = [
   {
@@ -24,12 +31,76 @@ const links = [
   },
 ];
 
+const socialLinks = [
+  {
+    Icon: Discord,
+    href: "https://todo.com",
+  },
+  {
+    Icon: Twitter,
+    href: "https://todo.com",
+  },
+  {
+    Icon: Github,
+    href: "https://todo.com",
+  },
+  {
+    Icon: Discourse,
+    href: "https://todo.com",
+  },
+  {
+    Icon: Message,
+    href: "https://todo.com",
+  },
+];
+
 export function MenuPanel() {
   return (
     <Wrapper>
       <Nav links={links} />
+      <FooterWrapper>
+        <SocialsWrapper>
+          <Socials>
+            {socialLinks.map(({ href, Icon }) => (
+              <Link key={href} href={href} passHref>
+                <A target="_blank">
+                  <SocialIconWrapper>
+                    <Icon />
+                  </SocialIconWrapper>
+                </A>
+              </Link>
+            ))}
+          </Socials>
+        </SocialsWrapper>
+        <PoweredByWrapper>
+          <PoweredBy>
+            <PoweredByText>Powered by</PoweredByText>
+            <UmaIconWrapper>
+              <Uma />
+            </UmaIconWrapper>
+          </PoweredBy>
+        </PoweredByWrapper>
+      </FooterWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div``;
+
+const FooterWrapper = styled.div``;
+
+const SocialsWrapper = styled.div``;
+
+const Socials = styled.div``;
+
+const A = styled.a``;
+
+const SocialIconWrapper = styled.div``;
+
+const PoweredByWrapper = styled.div``;
+
+const PoweredBy = styled.div``;
+
+const PoweredByText = styled.p``;
+
+const UmaIconWrapper = styled.div``;
