d373d82dcb26f563574e96cd5b36f70fd4769d06
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-03 11:11:17 +0200

    add address to create voting contract function
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import type { AppProps } from "next/app";
 import { GlobalStyle } from "components/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "styles/fonts.css";
 import { WalletProvider } from "contexts/WalletContext";
 import { PanelProvider } from "contexts/PanelContext";
 import { ContractsProvider } from "contexts/ContractsContext";
+import "styles/fonts.css";
 
 const queryClient = new QueryClient();
 
--- web3/createVotingContractInstance.ts
+++ web3/createVotingContractInstance.ts
@@ -2,7 +2,8 @@ import { VotingV2Ethers__factory } from "@uma/contracts-frontend";
 import { VotingV2Ethers } from "@uma/contracts-frontend";
 import { ethers } from "ethers";
 
-export default function createVotingContractInstance(signer: ethers.Signer, address: string) {
+export default function createVotingContractInstance(signer: ethers.Signer) {
+  const address = "0xf21d2478F75D405b6D1e368D313D06B8AD20E088";
   const instance: VotingV2Ethers = VotingV2Ethers__factory.connect(address, signer);
 
   return instance;
