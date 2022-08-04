a202d933a83353025b08407b66d3fb01e1db3a4d
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-04 13:58:57 +0200

    add use round end time hook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ hooks/useRoundEndTime.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { VotingV2Ethers } from "@uma/contracts-frontend";
+import { BigNumber } from "ethers";
+import getRoundEndTime from "web3/queries/getRoundEndTime";
+
+export default function useRoundEndTime(votingContract: VotingV2Ethers, roundId: BigNumber | undefined) {
+  const { isLoading, isError, data, error } = useQuery(["roundEndTime"], () =>
+    getRoundEndTime(votingContract, roundId)
+  );
+
+  return {
+    roundEndTime: data?.[0],
+    roundEndTimeIsLoading: isLoading,
+    roundEndTimeIsError: isError,
+    roundEndTimeError: error,
+  };
+}
