7cce7449eb3861f7418263e075d174d9653cc034
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-04 10:43:30 +0200

    use void signer voting contract as default value
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- contexts/ContractsContext.tsx
+++ contexts/ContractsContext.tsx
@@ -1,20 +1,23 @@
 import { VotingV2Ethers } from "@uma/contracts-frontend";
 import { createContext, ReactNode, useState } from "react";
+import createVotingContractInstance from "web3/createVotingContractInstance";
 
 interface ContractsContextState {
-  voting: VotingV2Ethers | null;
-  setVoting: (voting: VotingV2Ethers | null) => void;
+  voting: VotingV2Ethers;
+  setVoting: (voting: VotingV2Ethers) => void;
 }
 
+const defaultVoting = createVotingContractInstance();
+
 const defaultContractContextState = {
-  voting: null,
+  voting: defaultVoting,
   setVoting: () => null,
 };
 
 export const ContractsContext = createContext<ContractsContextState>(defaultContractContextState);
 
 export function ContractsProvider({ children }: { children: ReactNode }) {
-  const [voting, setVoting] = useState<VotingV2Ethers | null>(null);
+  const [voting, setVoting] = useState<VotingV2Ethers>(defaultVoting);
 
   return <ContractsContext.Provider value={{ voting, setVoting }}>{children}</ContractsContext.Provider>;
 }
