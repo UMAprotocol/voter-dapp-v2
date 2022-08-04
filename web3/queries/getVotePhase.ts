9f3c5c941c6157363db725a1b7f7f01e5d042759
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-04 11:02:20 +0200

    make get vote phase async
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/getVotePhase.ts
+++ web3/queries/getVotePhase.ts
@@ -1,5 +1,5 @@
 import { VotingV2Ethers } from "@uma/contracts-frontend";
 
-export default function getVotePhase(votingContract: VotingV2Ethers) {
-  return votingContract.functions.getVotePhase();
+export default async function getVotePhase(votingContract: VotingV2Ethers) {
+  return await votingContract.functions.getVotePhase();
 }
