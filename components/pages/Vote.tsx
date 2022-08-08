b04e332c19c7836dfee71e458810e31adad02d2c
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-08 09:29:55 +0200

    use live data in mock votes
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/pages/Vote.tsx
+++ components/pages/Vote.tsx
@@ -4,72 +4,79 @@ import { Layout } from "components/Layout";
 import { Votes } from "components/Votes";
 import { add, sub } from "date-fns";
 import { ethers } from "ethers";
+import useActiveVotes from "hooks/useActiveVotes";
+import { useContractsContext } from "hooks/useContractsContext";
 
 export function Vote() {
+  const { voting } = useContractsContext();
+  const { activeVotes } = useActiveVotes(voting);
+
   function makeMockVotes() {
-    const mockVotes = Array.from({ length: 5 });
-    return mockVotes.map((_, i) => ({
-      identifier: ethers.utils.hexlify(ethers.utils.toUtf8Bytes(`vote-${i}`)),
-      ancillaryData: ethers.utils.hexlify(ethers.utils.toUtf8Bytes(`ancillary-${i}`)),
-      decodedIdentifier: "vote-" + i,
-      decodedAncillaryData: "ancillary-" + i,
-      time: 1659700922602 / 1000,
-      timeMilliseconds: 1659700922602,
-      title: "Mock vote number: " + i,
-      origin: Math.random() > 0.5 ? ("UMA" as const) : ("Polymarket" as const),
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      voteNumber: 100 + i,
-      timestamp: sub(new Date(), { days: 1 }),
-      txid: "0x12345667890987655" + i,
-      umipNumber: 200 + i,
-      links: [
-        {
-          label: "UMIP link",
-          href: "https://www.todo.com",
-        },
-        {
-          label: "Dispute txid",
-          href: "https://www.todo.com",
-        },
-        {
-          label: "Optimistic Oracle UI",
-          href: "https://www.todo.com",
-        },
-      ],
-      discordLink: "https://www.todo.com",
-      options: [
-        { label: "Yes", value: "p0", secondaryLabel: "p0" },
-        { label: "No", value: "p1", secondaryLabel: "p1" },
-        { label: "Unknown", value: "p2", secondaryLabel: "p2" },
-        { label: "Early Request", value: "p3", secondaryLabel: "p3" },
-      ],
-      participation: [
-        { label: "Total Votes", value: 188077355.982231 },
-        { label: "Unique Commit Addresses", value: 100 },
-        { label: "Unique Reveal Addresses", value: 97 },
-      ],
-      results: [
-        {
-          label: "Devin Haney",
-          value: 1234,
-        },
-        {
-          label: "George Washington",
-          value: 5678,
-        },
-        {
-          label: "Tie",
-          value: 500,
-        },
-        {
-          label: "Early Expiry",
-          value: 199,
-        },
-      ],
-      isCommitted: Math.random() > 0.5,
-      isGovernance: Math.random() > 0.5,
-    }));
+    if (!activeVotes) return null;
+    return activeVotes.map(
+      ({ identifier, decodedIdentifier, ancillaryData, decodedAncillaryData, time, timeMilliseconds }, i) => ({
+        identifier,
+        ancillaryData,
+        decodedIdentifier,
+        decodedAncillaryData,
+        time,
+        timeMilliseconds,
+        title: decodedIdentifier,
+        origin: i % 2 === 0 ? ("UMA" as const) : ("Polymarket" as const),
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        voteNumber: 100 + i,
+        timestamp: sub(new Date(), { days: 1 }),
+        txid: "0x12345667890987655" + i,
+        umipNumber: 200 + i,
+        links: [
+          {
+            label: "UMIP link",
+            href: "https://www.todo.com",
+          },
+          {
+            label: "Dispute txid",
+            href: "https://www.todo.com",
+          },
+          {
+            label: "Optimistic Oracle UI",
+            href: "https://www.todo.com",
+          },
+        ],
+        discordLink: "https://www.todo.com",
+        options: [
+          { label: "Yes", value: "p0", secondaryLabel: "p0" },
+          { label: "No", value: "p1", secondaryLabel: "p1" },
+          { label: "Unknown", value: "p2", secondaryLabel: "p2" },
+          { label: "Early Request", value: "p3", secondaryLabel: "p3" },
+        ],
+        participation: [
+          { label: "Total Votes", value: 188077355.982231 },
+          { label: "Unique Commit Addresses", value: 100 },
+          { label: "Unique Reveal Addresses", value: 97 },
+        ],
+        results: [
+          {
+            label: "Devin Haney",
+            value: 1234,
+          },
+          {
+            label: "George Washington",
+            value: 5678,
+          },
+          {
+            label: "Tie",
+            value: 500,
+          },
+          {
+            label: "Early Expiry",
+            value: 199,
+          },
+        ],
+        isCommitted: i % 2 === 0,
+        isGovernance: i % 2 === 0,
+      })
+    );
   }
 
   const mockVotes = makeMockVotes();
@@ -87,7 +94,7 @@ export function Vote() {
     <Layout>
       <Banner />
       <HowItWorks {...mockStakeholderData} />
-      <Votes votes={mockVotes} />
+      {mockVotes && <Votes votes={mockVotes!} />}
     </Layout>
   );
 }
