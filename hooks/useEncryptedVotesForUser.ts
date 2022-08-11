a66e56beb6e4773a939223fd66551cac14d5f3e7
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-11 08:27:36 +0200

    map out encrypted votes for user in one step
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/useEncryptedVotesForUser.ts
+++ hooks/useEncryptedVotesForUser.ts
@@ -1,57 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import { VotingV2Ethers } from "@uma/contracts-frontend";
-import { TypedEvent } from "@uma/contracts-frontend/dist/typechain/core/ethers/commons";
 import { BigNumber } from "ethers";
+import { VoteT } from "types/global";
 import getEncryptedVotesForUser from "web3/queries/getEncryptedVotesForUser";
 
 export default function useEncryptedVotesForUser(
   votingContract: VotingV2Ethers,
   address: string,
-  roundId: BigNumber | undefined
+  roundId: BigNumber | undefined,
+  votes: VoteT[]
 ) {
   const { isLoading, isError, data, error } = useQuery(
     ["encryptedVotesForUser"],
     () => getEncryptedVotesForUser(votingContract, address, roundId),
     {
       refetchInterval: 1000,
+      enabled: !!roundId,
     }
   );
 
-  const encryptedVotesForUser = makeEncryptedVotesForUser(data, address);
+  const encryptedVotesForUser = data
+    ?.map(({ args }) => args)
+    ?.map(({ roundId, caller, identifier, time, ancillaryData, encryptedVote }) => ({
+      roundId,
+      caller,
+      identifier,
+      time,
+      ancillaryData,
+      encryptedVote,
+    }));
+
+  const votesUserVotedOn = votes?.filter((vote) => {
+    return Boolean(
+      encryptedVotesForUser?.find(({ identifier, ancillaryData, time }) => {
+        return vote.identifier === identifier && vote.ancillaryData === ancillaryData && vote.time === time.toNumber();
+      })
+    );
+  });
 
   return {
     encryptedVotesForUser,
+    votesUserVotedOn,
     encryptedVotesForUserIsLoading: isLoading,
     encryptedVotesForUserIsError: isError,
     encryptedVotesForUserError: error,
   };
-}
-
-function makeEncryptedVotesForUser(
-  encryptedVoteEvents:
-    | TypedEvent<
-        [string, BigNumber, string, BigNumber, string, string] & {
-          caller: string;
-          roundId: BigNumber;
-          identifier: string;
-          time: BigNumber;
-          ancillaryData: string;
-          encryptedVote: string;
-        }
-      >[]
-    | undefined,
-  address: string
-) {
-  if (!encryptedVoteEvents) return [];
-
-  const args = encryptedVoteEvents.map(({ args }) => args);
-
-  return args.map(({ roundId, caller, identifier, time, ancillaryData, encryptedVote }) => ({
-    roundId,
-    caller,
-    identifier,
-    time,
-    ancillaryData,
-    encryptedVote,
-  }))
 }
