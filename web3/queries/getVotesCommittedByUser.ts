532b9cb29872e1fc9470ecee0e8ce26a81492ac8
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-12 14:17:01 +0200

    add votes committed by user query
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ web3/queries/getVotesCommittedByUser.ts
@@ -0,0 +1,6 @@
+import { VotingV2Ethers } from "@uma/contracts-frontend";
+
+export default function getVotesRevealedByUser(votingContract: VotingV2Ethers, address: string) {
+  const filter = votingContract.filters.VoteRevealed(address, null, null, null, null, null, null, null);
+  return votingContract.queryFilter(filter);
+}
