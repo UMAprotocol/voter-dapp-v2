958f0c37cef79be378b4be34e36d5d0e50691db6
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-16 14:42:02 +0200

    add staked balance hook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ hooks/useStakedBalance.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { VotingV2Ethers } from "@uma/contracts-frontend";
+import { ethers } from "ethers";
+import getStakedBalance from "web3/queries/getStakedBalance";
+
+export default function useStakedBalance(votingContract: VotingV2Ethers, address: string) {
+  const { isLoading, isError, data, error } = useQuery(
+    ["stakedBalance"],
+    () => getStakedBalance(votingContract, address),
+    {
+      refetchInterval(data) {
+        return data ? false : 1000;
+      },
+    }
+  );
+
+  return {
+    stakedBalance: ethers.utils.formatEther(data?.[0] ?? 0),
+    stakedBalanceIsLoading: isLoading,
+    stakedBalanceIsError: isError,
+    stakedBalanceError: error,
+  };
+}
