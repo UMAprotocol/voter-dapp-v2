bfdc9c19e1f071672c9a976e179600c7c259c045
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-17 09:08:18 +0200

    add stake mutation
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ web3/mutations/stake.ts
@@ -0,0 +1,7 @@
+import { VotingV2Ethers } from "@uma/contracts-frontend";
+import { ethers } from "ethers";
+
+export default async function stake({ voting, stakeAmount }: { voting: VotingV2Ethers; stakeAmount: string }) {
+  const tx = await voting.functions.stake(ethers.utils.parseEther(stakeAmount));
+  return await tx.wait(1);
+}
