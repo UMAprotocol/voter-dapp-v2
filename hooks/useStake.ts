3b41e349e65445efe159ed54decd6411ae0b4b2a
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-17 11:26:23 +0200

    add update logic to stake mutation hook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/useStake.ts
+++ hooks/useStake.ts
@@ -1,14 +1,26 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { BigNumber, ethers } from "ethers";
 import stake from "web3/mutations/stake";
 
 export default function useStake() {
   const queryClient = useQueryClient();
   const { mutate } = useMutation(stake, {
     onSuccess: (_data, { stakeAmount }) => {
-      queryClient.setQueryData<number>(["stakedBalance"], Number(stakeAmount));
-      queryClient.setQueryData<number>(["unstakedBalance"], (oldUnstakedBalance) =>
-        oldUnstakedBalance ? oldUnstakedBalance - Number(stakeAmount) : oldUnstakedBalance
-      );
+      const parsedStakeAmount = ethers.utils.parseEther(stakeAmount);
+
+      queryClient.setQueryData<[BigNumber]>(["stakedBalance"], (oldStakedBalance) => {
+        if (!oldStakedBalance) return undefined;
+
+        const newStakedBalance = oldStakedBalance[0].add(parsedStakeAmount);
+        return [newStakedBalance];
+      });
+
+      queryClient.setQueryData<[BigNumber]>(["unstakedBalance"], (oldUnstakedBalance) => {
+        if (!oldUnstakedBalance) return undefined;
+
+        const newUnstakedBalance = oldUnstakedBalance[0].sub(parsedStakeAmount);
+        return [newUnstakedBalance];
+      });
     },
   });
   return mutate;
