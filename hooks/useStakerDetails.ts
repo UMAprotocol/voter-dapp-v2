a92a13791c140b6420f5a2cdc83187d743f8a6ee
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-18 13:28:19 +0200

    add staker details hook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ hooks/useStakerDetails.ts
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import { VotingV2Ethers } from "@uma/contracts-frontend";
+import { ethers } from "ethers";
+import getStakerDetails from "web3/queries/getStakerDetails";
+
+export default function useStakerDetails(votingContract: VotingV2Ethers, address: string | undefined) {
+  const { isLoading, isError, data, error } = useQuery(
+    ["stakerDetails"],
+    () => getStakerDetails(votingContract, address),
+    {
+      refetchInterval: (data) => (data ? false : 1000),
+      enabled: !!address,
+    }
+  );
+
+  const { outstandingRewards, pendingUnstake, rewardsPaidPerToken, unstakeRequestTime } = data ?? {};
+
+  const stakerDetails = {
+    outstandingRewards: Number(ethers.utils.formatEther(outstandingRewards ?? 0)),
+    pendingUnstake: Number(ethers.utils.formatEther(pendingUnstake ?? 0)),
+    rewardsPaidPerToken: Number(ethers.utils.formatEther(rewardsPaidPerToken ?? 0)),
+    unstakeRequestTime: new Date(Number(unstakeRequestTime?.toNumber() ?? 0) * 1000),
+  };
+
+  return {
+    stakerDetails,
+    stakerDetailsIsLoading: isLoading,
+    stakerDetailsIsError: isError,
+    stakerDetailsError: error,
+  };
+}
