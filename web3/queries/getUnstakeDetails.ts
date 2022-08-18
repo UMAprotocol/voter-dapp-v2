dd229ce78fdabc6327714999910afde91bdcf64a
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-18 13:27:35 +0200

    rename for clarity
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/getUnstakeDetails.ts
+++
@@ -1,6 +0,0 @@
-import { VotingV2Ethers } from "@uma/contracts-frontend";
-
-export default function getUnstakeDetails(votingContract: VotingV2Ethers, address: string | undefined) {
-  if (!address) return undefined;
-  return votingContract.voterStakes(address);
-}
