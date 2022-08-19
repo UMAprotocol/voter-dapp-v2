1c42da294aacc29213df902df78ca1b806e5b2d0
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-19 13:19:12 +0200

    add transaction hash when fetching requests
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/useActiveVotes.ts
+++ hooks/useActiveVotes.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { VotingV2Ethers } from "@uma/contracts-frontend";
 import { decodeHexString } from "helpers/decodeHexString";
 import { makeUniqueKeyForVote } from "helpers/votes";
-import { PriceRequest } from "types/global";
+import { PriceRequestT } from "types/global";
 import getPendingRequests from "web3/queries/getPendingRequests";
 
 export default function useActiveVotes(votingContract: VotingV2Ethers) {
@@ -14,11 +14,13 @@ export default function useActiveVotes(votingContract: VotingV2Ethers) {
 
   const pendingRequests = data?.[0];
 
-  const activeVotes: PriceRequest[] | undefined =
+  const activeVotes: PriceRequestT[] | undefined =
     pendingRequests?.map(({ time, identifier, ancillaryData }) => ({
       time: time.toNumber(),
       identifier,
       ancillaryData,
+      // todo: replace with on chain value once getter is updated
+      transactionHash: "0x5b80ae07dfec789436ce29ff8169907e6ad4dcf765244314fb3748d8c7042925",
       timeMilliseconds: time.toNumber() * 1000,
       timeAsDate: new Date(time.toNumber() * 1000),
       decodedIdentifier: decodeHexString(identifier),
