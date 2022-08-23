bcbc460f04ca028d2614eed28900321bb73c1c3b
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-23 15:48:15 +0200

    remove unused hooks
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/usePriceRequestRounds.ts
+++
@@ -1,6 +0,0 @@
-import getPriceRequestRounds from "graph/queries/getPriceRequestRounds";
-import { useQuery } from "@tanstack/react-query";
-
-export default function usePriceRequestRounds(orderBy: string, orderDirection: string, numToQuery: number) {
-  return useQuery(["priceRequestRounds"], () => getPriceRequestRounds(orderBy, orderDirection, numToQuery));
-}
