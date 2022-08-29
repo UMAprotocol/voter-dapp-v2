a3080522c4ad15cd104048ab9dbddc1df3bc0fa2
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-29 12:08:01 +0200

    remove redundant contract queries
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/getCurrentRoundId.ts
+++
@@ -1,5 +0,0 @@
-import { VotingV2Ethers } from "@uma/contracts-frontend";
-
-export default function getCurrentRoundId(votingContract: VotingV2Ethers) {
-  return votingContract.functions.getCurrentRoundId();
-}
