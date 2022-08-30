c2bcf85d374ac4fcdfad7d644eef586abe7984db
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-30 15:23:11 +0200

    update encrypted votes in commit mutation
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/mutations/useCommitVotes.ts
+++ hooks/mutations/useCommitVotes.ts
@@ -1,20 +1,30 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { committedVotesKey } from "constants/queryKeys";
-import { VoteExistsByKeyT } from "types/global";
+import { committedVotesKey, encryptedVotesKey } from "constants/queryKeys";
+import { EncryptedVotesByKeyT, VoteExistsByKeyT } from "types/global";
 import { commitVotes } from "web3/mutations";
 
 export default function useCommitVotes() {
   const queryClient = useQueryClient();
   const { mutate } = useMutation(commitVotes, {
-    onSuccess: (_data, { selectedVotes }) => {
+    onSuccess: (_data, { formattedVotes }) => {
       queryClient.setQueryData<VoteExistsByKeyT>([committedVotesKey], (oldCommittedVotes) => {
         const newCommittedVotes = { ...oldCommittedVotes };
 
-        for (const uniqueKey of Object.keys(selectedVotes)) {
+        for (const { uniqueKey } of formattedVotes) {
           newCommittedVotes[uniqueKey] = true;
         }
 
         return newCommittedVotes;
+      });
+
+      queryClient.setQueryData<EncryptedVotesByKeyT>([encryptedVotesKey], (oldEncryptedVotes) => {
+        const newEncryptedVotes = { ...oldEncryptedVotes };
+
+        for (const { uniqueKey, encryptedVote } of formattedVotes) {
+          newEncryptedVotes[uniqueKey] = encryptedVote;
+        }
+
+        return newEncryptedVotes;
       });
     },
   });
