22f32b57a7577556b9acdde05be3e2b4051c8edf
ryanwolhuter <dev@ryanwolhuter.com>
2022-08-30 13:35:05 +0200

    cleaner export syntax
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/mutations/index.ts
+++ hooks/mutations/index.ts
@@ -1,8 +1,8 @@
-import useApprove from "./useApprove";
-import useExecuteUnstake from "./useExecuteUnstake";
-import useRequestUnstake from "./useRequestUnstake";
-import useStake from "./useStake";
-import useWithdrawAndRestake from "./useWithdrawAndRestake";
-import useWithdrawRewards from "./useWithdrawRewards";
-
-export { useApprove, useExecuteUnstake, useRequestUnstake, useStake, useWithdrawAndRestake, useWithdrawRewards };
+export { default as useApprove } from "./useApprove";
+export { default as useCommitVotes } from "./useCommitVotes";
+export { default as useExecuteUnstake } from "./useExecuteUnstake";
+export { default as useRequestUnstake } from "./useRequestUnstake";
+export { default as useRevealVotes } from "./useRevealVotes";
+export { default as useStake } from "./useStake";
+export { default as useWithdrawAndRestake } from "./useWithdrawAndRestake";
+export { default as useWithdrawRewards } from "./useWithdrawRewards";
