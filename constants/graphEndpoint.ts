f72d17d144375170d97856671e16013735917b3b
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-02 10:34:52 +0200

    update graph endpoint
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- constants/graphEndpoint.ts
+++ constants/graphEndpoint.ts
@@ -1,3 +1,3 @@
-const graphEndpoint = "https://api.thegraph.com/subgraphs/name/md0x/votingv2-version-11";
+const graphEndpoint = "https://api.thegraph.com/subgraphs/name/md0x/votingv2-version-16";
 
 export default graphEndpoint;
