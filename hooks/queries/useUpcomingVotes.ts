cc671d3ac56b79b1f8c47d793d308767cf054d3b
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-05 13:47:35 +0200

    return has upcoming votes as a boolean in getter
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/queries/useUpcomingVotes.ts
+++ hooks/queries/useUpcomingVotes.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { upcomingVotesKey } from "constants/queryKeys";
+import makePriceRequestsByKey from "helpers/makePriceRequestsByKey";
 import { useContractsContext, useVoteTimingContext } from "hooks/contexts";
 import { getUpcomingVotes } from "web3/queries";
 import useHasActiveVotes from "./useHasActiveVotes";
-import makePriceRequestsByKey from "helpers/makePriceRequestsByKey";
 
 export default function useUpcomingVotes() {
   const { voting } = useContractsContext();
@@ -20,8 +20,9 @@ export default function useUpcomingVotes() {
   const eventData = data?.map(({ args }) => args);
   const onlyUpcoming = eventData?.filter((event) => event.roundId.toNumber() > roundId);
   const upcomingVotes = makePriceRequestsByKey(onlyUpcoming);
-
+  const hasUpcomingVotes = isLoading ? undefined : Object.keys(upcomingVotes).length > 0;
   return {
+    hasUpcomingVotes,
     upcomingVotes,
     upcomingVotesIsLoading: isLoading,
     upcomingVotesIsError: isError,
