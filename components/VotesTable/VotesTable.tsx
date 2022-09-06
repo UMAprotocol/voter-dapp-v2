9d095d444f8da5876e92da8a749358ca7646b3c8
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-06 12:47:39 +0200

    add votes table component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/VotesTable/VotesTable.tsx
+++ components/VotesTable/VotesTable.tsx
@@ -1,3 +1,24 @@
-export default function VotesTable() {
-  return <div></div>;
+import styled from "styled-components";
+
+interface Props {
+  headings: JSX.Element;
+  rows: JSX.Element[];
 }
+export default function VotesTable({ headings, rows }: Props) {
+  return (
+    <Table>
+      <Thead>{headings}</Thead>
+      <Tbody>{rows}</Tbody>
+    </Table>
+  );
+}
+
+const Table = styled.table`
+  table-layout: fixed;
+  width: 100%;
+  border-spacing: 30px 5px;
+`;
+
+const Thead = styled.thead``;
+
+const Tbody = styled.tbody``;
