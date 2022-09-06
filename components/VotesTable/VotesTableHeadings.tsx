0cd6250ebd36186f2772ab46ebaef5b4d2d92585
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-06 12:47:29 +0200

    extract votes table headings to component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ components/VotesTable/VotesTableHeadings.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { ActivityStatusT } from "types/global";
+
+interface Props {
+  activityStatus: ActivityStatusT;
+}
+export default function VotesTableHeadings({ activityStatus }: Props) {
+  const activeHeadings = ["Vote", "Your vote", "Vote status", ""];
+  const upcomingHeadings = ["Vote", ""];
+  const pastHeadings = ["Vote", "Your vote", "Correct vote", ""];
+  const headings =
+    activityStatus === "active" ? activeHeadings : activityStatus === "upcoming" ? upcomingHeadings : pastHeadings;
+  return (
+    <Wrapper>
+      {headings.map((heading) => (
+        <Heading scope="col" key={heading}>
+          {heading}
+        </Heading>
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.tr``;
+
+const Heading = styled.th`
+  text-align: left;
+  font: var(--text-sm);
+  &:first-child {
+    width: 45%;
+  }
+  &:nth-child(2) {
+    width: 240px;
+  }
+`;
