40ea17236e74d3e2800eb9c0300524889054648c
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-06 09:04:14 +0200

    add vote mock data
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/Pages/VotePage/VoteBar.stories.tsx
+++ stories/Pages/VotePage/VoteBar.stories.tsx
@@ -1,7 +1,14 @@
 import { Meta, Story } from "@storybook/react";
 import { Props, VoteBar } from "components/VoteBar";
 import { defaultVoteTimingContextState, VoteTimingContext } from "contexts/VoteTimingContext";
-import { sub } from "date-fns";
+import {
+  voteCommitted,
+  voteCommittedButNotRevealed,
+  voteRevealed,
+  voteWithCorrectVoteWithoutUserVote,
+  voteWithCorrectVoteWithUserVote,
+  voteWithoutUserVote,
+} from "stories/mocks/votes";
 import { VotePhaseT } from "types/global";
 
 interface StoryProps extends Props {
@@ -28,82 +35,6 @@ const Template: Story<StoryProps> = (args) => {
     </VoteTimingContext.Provider>
   );
 };
-
-const voteWithoutUserVote = {
-  isCommitted: false,
-  title: "SuperUMAn DAO KPI Options funding proposal",
-  origin: "UMA" as const,
-  description: "Some description",
-  transactionHash: "0x1234567890",
-  umipUrl: "https://uma.io",
-  timeAsDate: sub(new Date(), { days: 1 }),
-  time: sub(new Date(), { days: 1 }).getTime() / 1000,
-  timeMilliseconds: sub(new Date(), { days: 1 }).getTime(),
-  identifier: "0x1234567890",
-  ancillaryData: "0x1234567890",
-  decodedIdentifier: "SuperUMAn DAO KPI Options funding proposal",
-  decodedAncillaryData: "Test test test",
-  uniqueKey: "0x1234567890",
-  umipNumber: 20,
-  encryptedVote: undefined,
-  decryptedVote: undefined,
-  contentfulData: undefined,
-  options: [
-    { label: "Yes", value: "0", secondaryLabel: "p0" },
-    { label: "No", value: "1", secondaryLabel: "p1" },
-  ],
-  links: [
-    {
-      label: "UMIP link",
-      href: "https://www.todo.com",
-    },
-    {
-      label: "Dispute txid",
-      href: "https://www.todo.com",
-    },
-    {
-      label: "Optimistic Oracle UI",
-      href: "https://www.todo.com",
-    },
-  ],
-  discordLink: "https://www.todo.com",
-  isGovernance: false,
-  isRevealed: false,
-};
-
-const userVote = {
-  encryptedVote: "0x0",
-  decryptedVote: {
-    price: "0",
-    salt: "0",
-  },
-};
-
-const voteWithUserVote = {
-  ...voteWithoutUserVote,
-  ...userVote,
-};
-
-const voteCommitted = {
-  ...voteWithUserVote,
-  isCommitted: true,
-};
-
-const voteCommittedButNotRevealed = { ...voteCommitted };
-
-const voteRevealed = { ...voteCommittedButNotRevealed, isRevealed: true };
-
-const voteWithCorrectVoteWithoutUserVote = {
-  ...voteWithoutUserVote,
-  correctVote: 0,
-};
-
-const voteWithCorrectVoteWithUserVote = {
-  ...voteWithUserVote,
-  correctVote: 0,
-};
-
-const longTitle = "Will Coinbase support Polygon USDC deposits & withdrawals by June 30, 2022?";
 
 const mockMoreDetailsAction = () => alert("More details clicked");
 
---
+++ stories/mocks/votes.ts
@@ -0,0 +1,75 @@
+import { sub } from "date-fns";
+
+export const voteWithoutUserVote = {
+  isCommitted: false,
+  title: "SuperUMAn DAO KPI Options funding proposal",
+  origin: "UMA" as const,
+  description: "Some description",
+  transactionHash: "0x1234567890",
+  umipUrl: "https://uma.io",
+  timeAsDate: sub(new Date(), { days: 1 }),
+  time: sub(new Date(), { days: 1 }).getTime() / 1000,
+  timeMilliseconds: sub(new Date(), { days: 1 }).getTime(),
+  identifier: "0x1234567890",
+  ancillaryData: "0x1234567890",
+  decodedIdentifier: "SuperUMAn DAO KPI Options funding proposal",
+  decodedAncillaryData: "Test test test",
+  uniqueKey: "0x1234567890",
+  umipNumber: 20,
+  encryptedVote: undefined,
+  decryptedVote: undefined,
+  contentfulData: undefined,
+  options: [
+    { label: "Yes", value: "0", secondaryLabel: "p0" },
+    { label: "No", value: "1", secondaryLabel: "p1" },
+  ],
+  links: [
+    {
+      label: "UMIP link",
+      href: "https://www.todo.com",
+    },
+    {
+      label: "Dispute txid",
+      href: "https://www.todo.com",
+    },
+    {
+      label: "Optimistic Oracle UI",
+      href: "https://www.todo.com",
+    },
+  ],
+  discordLink: "https://www.todo.com",
+  isGovernance: false,
+  isRevealed: false,
+};
+
+export const userVote = {
+  encryptedVote: "0x0",
+  decryptedVote: {
+    price: "0",
+    salt: "0",
+  },
+};
+
+export const voteWithUserVote = {
+  ...voteWithoutUserVote,
+  ...userVote,
+};
+
+export const voteCommitted = {
+  ...voteWithUserVote,
+  isCommitted: true,
+};
+
+export const voteCommittedButNotRevealed = { ...voteCommitted };
+
+export const voteRevealed = { ...voteCommittedButNotRevealed, isRevealed: true };
+
+export const voteWithCorrectVoteWithoutUserVote = {
+  ...voteWithoutUserVote,
+  correctVote: 0,
+};
+
+export const voteWithCorrectVoteWithUserVote = {
+  ...voteWithUserVote,
+  correctVote: 0,
+};
