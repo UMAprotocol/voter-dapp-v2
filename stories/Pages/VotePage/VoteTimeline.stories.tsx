3beb515b623d49210d44bc0916395ed4c9db9e73
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-19 11:29:13 +0200

    add timing context to timeline stories
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/Pages/VotePage/VoteTimeline.stories.tsx
+++ stories/Pages/VotePage/VoteTimeline.stories.tsx
@@ -1,21 +1,107 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 import { VoteTimeline } from "components/VoteTimeline";
-import add from "date-fns/add";
+import { defaultVotesContextState, VotesContext } from "contexts/VotesContext";
+import { defaultVoteTimingContextState, VoteTimingContext } from "contexts/VoteTimingContext";
+
+interface StoryProps {
+  phase: "commit" | "reveal";
+  millisecondsUntilPhaseEnds: number;
+  activityStatus: "active" | "upcoming" | "past";
+}
+
+const defaultArgs = {
+  millisecondsUntilPhaseEnds: 1000 * 60 * 5,
+};
+
+const commitPhaseDefaultArgs = {
+  ...defaultArgs,
+  phase: "commit" as const,
+};
+
+const revealPhaseDefaultArgs = {
+  ...defaultArgs,
+  phase: "reveal" as const,
+};
 
 export default {
   title: "Pages/Vote Page/VoteTimeline",
   component: VoteTimeline,
-} as ComponentMeta<typeof VoteTimeline>;
+  argTypes: {
+    phase: {
+      options: ["commit", "reveal"],
+      control: {
+        type: "radio",
+      },
+    },
+    activityStatus: {
+      options: ["active", "upcoming", "past"],
+      control: {
+        type: "radio",
+      },
+    },
+    millisecondsUntilPhaseEnds: {
+      control: {
+        type: "range",
+        min: 0,
+        max: 1000 * 60 * 10,
+        step: 1000,
+      },
+    },
+  },
+} as Meta<StoryProps>;
 
-const Template: ComponentStory<typeof VoteTimeline> = () => <VoteTimeline />;
+const Template: Story<StoryProps> = (args) => {
+  const mockVoteTimingContextState = {
+    ...defaultVoteTimingContextState,
+    phase: args.phase ?? "commit",
+    millisecondsUntilPhaseEnds: args.millisecondsUntilPhaseEnds,
+  };
 
-export const CommitPhase = Template.bind({});
-CommitPhase.args = {
-  phase: "commit",
-  phaseEnds: add(new Date(), { minutes: 59 }),
+  const mockVotesContextState = {
+    ...defaultVotesContextState,
+    getActivityStatus: () => args.activityStatus ?? "active",
+  };
+  return (
+    <VoteTimingContext.Provider value={mockVoteTimingContextState}>
+      <VotesContext.Provider value={mockVotesContextState}>
+        <VoteTimeline />
+      </VotesContext.Provider>
+    </VoteTimingContext.Provider>
+  );
 };
-export const RevealPhase = Template.bind({});
-RevealPhase.args = {
-  phase: "reveal",
-  phaseEnds: add(new Date(), { hours: 23, minutes: 59 }),
+
+export const CommitPhaseActive = Template.bind({});
+CommitPhaseActive.args = {
+  ...commitPhaseDefaultArgs,
+  activityStatus: "active",
+};
+
+export const CommitPhaseUpcoming = Template.bind({});
+CommitPhaseUpcoming.args = {
+  ...commitPhaseDefaultArgs,
+  activityStatus: "upcoming",
+};
+
+export const CommitPhasePast = Template.bind({});
+CommitPhasePast.args = {
+  ...commitPhaseDefaultArgs,
+  phase: "commit",
+};
+
+export const RevealPhaseActive = Template.bind({});
+RevealPhaseActive.args = {
+  ...revealPhaseDefaultArgs,
+  activityStatus: "active",
+};
+
+export const RevealPhaseUpcoming = Template.bind({});
+RevealPhaseUpcoming.args = {
+  ...revealPhaseDefaultArgs,
+  activityStatus: "upcoming",
+};
+
+export const RevealPhasePast = Template.bind({});
+RevealPhasePast.args = {
+  ...revealPhaseDefaultArgs,
+  activityStatus: "past",
 };
