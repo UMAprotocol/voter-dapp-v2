3edb03f88735a6642e5cac4a25873e8a064ff639
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-19 13:29:52 +0200

    use env vars for addresses
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -1,2 +1,2 @@
-export const votingAddress = "0x11680368E6a4F83Db1855629782f7b78F470fF62";
-export const votingTokenAddress = "0xBa950974AFdae520cEE596c98295150Cf8D9659c";
+export const votingAddress = process.env.NEXT_PUBLIC_VOTING_CONTRACT_ADDRESS_GOERLI;
+export const votingTokenAddress = process.env.NEXT_PUBLIC_VOTING_TOKEN_CONTRACT_ADDRESS_GOERLI;
