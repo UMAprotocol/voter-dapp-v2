52248ac14a69af1006b9e8012b96d370041112a6
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-21 10:10:24 +0200

    return zero for balances when wallet is not connected
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/getTokenAllowance.ts
+++ web3/queries/getTokenAllowance.ts
@@ -1,7 +1,10 @@
 import { VotingTokenEthers } from "@uma/contracts-frontend";
 import { votingContractAddress } from "constants/addresses";
+import { BigNumber } from "ethers";
 
 export default async function getTokenAllowance(votingTokenContract: VotingTokenEthers, address: string) {
+  if (!address) return BigNumber.from(0);
+
   const result = await votingTokenContract.functions.allowance(address, votingContractAddress);
   return result?.[0];
 }
