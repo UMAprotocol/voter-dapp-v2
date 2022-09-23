4c3e27863a590e8d01401f7c78ced5e36ec8d0d8
Ryan Wolhuter <dev@ryanwolhuter.com>
2022-09-23 09:32:53 +0200

    improve: strengthen ancillary data assumptions (#20)
    * add is polymarket function
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * add check if is polymarket helper function
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * show decoded ancillary data in results panel
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    * add decoded ancillary data to vote panel story
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>
    
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ helpers/checkIfIsPolymarket.ts
@@ -0,0 +1,10 @@
+export default function checkIfIsPolymarket(decodedIdentifier: string, decodedAncillaryData: string) {
+  const queryTitleToken = "q: title:";
+  const resultDataToken = "res_data:";
+  const isPolymarket =
+    decodedIdentifier === "YES_OR_NO_QUERY" &&
+    decodedAncillaryData.includes(queryTitleToken) &&
+    decodedAncillaryData.includes(resultDataToken);
+
+  return isPolymarket;
+}
