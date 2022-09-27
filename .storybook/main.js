18668d6f1e7e007e42b111e9ec68a3f4d926106b
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-27 15:50:28 +0200

    add browser polyfills for node functions in storybook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -34,8 +36,23 @@ module.exports = {
       }
     })
 
-    // polyfill needed for web3onboard
-    config.resolve.fallback.stream = require.resolve('stream-browserify')
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      assert: require.resolve("assert"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify"),
+      url: require.resolve("url"),
+    });
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ]);
 
     return config
   }
