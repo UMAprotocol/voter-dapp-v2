67eafc6a53d1c025a71511ff2f229683ea83bd52
ryanwolhuter <dev@ryanwolhuter.com>
2022-09-28 16:17:14 +0200

    add request index to votes
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- helpers/makePriceRequestsByKey.ts
+++ helpers/makePriceRequestsByKey.ts
@@ -23,9 +23,9 @@ function formatPriceRequest(priceRequest: RawPriceRequestDataT) {
   const time = Number(priceRequest.time);
   const timeMilliseconds = time * 1000;
   const timeAsDate = new Date(timeMilliseconds);
-  const transactionHash = "0x5b80ae07dfec789436ce29ff8169907e6ad4dcf765244314fb3748d8c7042925";
   const identifier = priceRequest.identifier;
   const ancillaryData = priceRequest.ancillaryData;
+  const voteNumber = priceRequest.priceRequestIndex;
   const decodedIdentifier = decodeHexString(identifier);
   const decodedAncillaryData = decodeHexString(ancillaryData);
   const correctVote = priceRequest.correctVote;
@@ -35,7 +35,7 @@ function formatPriceRequest(priceRequest: RawPriceRequestDataT) {
     time,
     identifier,
     ancillaryData,
-    transactionHash,
+    voteNumber,
     timeMilliseconds,
     timeAsDate,
     decodedIdentifier,
