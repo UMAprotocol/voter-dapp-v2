6a5060f831bc41bdfc20b81c04388d7169ccc38b
ryanwolhuter <dev@ryanwolhuter.com>
2022-10-03 13:15:34 +0200

    add is ether option to format number
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- helpers/formatNumber.ts
+++ helpers/formatNumber.ts
@@ -1,13 +1,25 @@
-import { BigNumber } from "ethers";
+import { BigNumber, FixedNumber } from "ethers";
 import { commify, formatEther } from "helpers";
 
-export function formatNumberForDisplay(number: BigNumber | undefined, decimals = 2) {
+export function formatNumberForDisplay(
+  number: BigNumber | undefined,
+  options?: { decimals?: number; isEther?: boolean }
+) {
   if (!number) return "0";
-  return truncateDecimals(commify(formatEther(number)), decimals);
+  const { decimals = 2, isEther = true } = options || {};
+  const _number = isEther ? formatEther(number) : number.toString();
+  return truncateDecimals(commify(_number), decimals);
 }
 
 export function truncateDecimals(number: string, decimals: number) {
   const [whole, decimal] = number.split(".");
   if (!decimal) return number;
   return `${whole}.${decimal.slice(0, decimals)}`;
+}
+
+export function bigNumberFromDecimal(decimal: string | number) {
+  if (typeof decimal === "number") {
+    decimal = decimal.toFixed(18);
+  }
+  return BigNumber.from(FixedNumber.from(truncateDecimals(decimal, 18)));
 }
