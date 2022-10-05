f41d5f000056b8d8bbe5b4892b60019198cf43d5
ryanwolhuter <dev@ryanwolhuter.com>
2022-10-05 13:18:58 +0200

    generalize banner
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/pages/Vote.tsx
+++ components/pages/Vote.tsx
@@ -1,11 +1,18 @@
 import { Banner, HowItWorks, Layout, Votes } from "components";
+import styled from "styled-components";
 
 export function Vote() {
   return (
     <Layout>
-      <Banner />
+      <Banner>
+        Stake, vote &amp; earn up to <Strong>30% APY</Strong>
+      </Banner>
       <HowItWorks />
       <Votes />
     </Layout>
   );
 }
+
+const Strong = styled.strong`
+  color: var(--red-500);
+`;
