b841e71a5c6ae8e2fc70e6f4199c75901bcf4ae7
ryanwolhuter <dev@ryanwolhuter.com>
2022-10-13 10:54:13 +0200

    add pending requests conditional render logic
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Panel/DelegationPanel.tsx
+++ components/Panel/DelegationPanel.tsx
@@ -1,11 +1,14 @@
 import { Button, PanelErrorBanner, TextInput } from "components";
 import { getAddress, isAddress } from "helpers";
 import { useErrorContext, useUserContext } from "hooks";
+import { useDelegationContext } from "hooks/contexts/useDelegationContext";
+import NextLink from "next/link";
 import One from "public/assets/icons/one.svg";
 import Three from "public/assets/icons/three.svg";
+import Time from "public/assets/icons/time-with-inner-circle.svg";
 import Two from "public/assets/icons/two.svg";
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { PanelFooter } from "./PanelFooter";
 import { PanelTitle } from "./PanelTitle";
 import { PanelSectionText, PanelSectionTitle, PanelWrapper } from "./styles";
@@ -14,6 +17,19 @@ export function DelegationPanel() {
   const { address } = useUserContext();
   const { addErrorMessage, clearErrorMessages } = useErrorContext("delegation");
   const [delegateAddressToAdd, setDelegateAddressToAdd] = useState("");
+  const { getDelegationStatus, getPendingSetDelegateRequestsForDelegator } = useDelegationContext();
+
+  const delegationStatus = getDelegationStatus();
+
+  // don't show this panel for users who have accepted a delegation request
+  if (delegationStatus === "delegate") return null;
+
+  const pendingRequests = getPendingSetDelegateRequestsForDelegator();
+
+  // only allow user to add a delegate wallet if they are neither a delegator nor a delegate
+  const showAddDelegateInput = delegationStatus === "no-delegation";
+
+  const showPendingRequests = delegationStatus === "delegator-pending" && pendingRequests.length > 0;
 
   function onInput(inputAddress: string) {
     setDelegateAddressToAdd(inputAddress);
@@ -58,10 +74,29 @@ export function DelegationPanel() {
             You now have a secondary delegation wallet where you can vote text text
           </StepWrapper>
         </StepsWrapper>
-        <WalletInputWrapper>
-          <TextInput value={delegateAddressToAdd} onInput={onInput} placeholder="Enter delegate address" />
-          <Button variant="primary" label="Add delegate wallet" onClick={() => {}} height={45} width="100%" />
-        </WalletInputWrapper>
+        {showAddDelegateInput && (
+          <AddDelegateInputWrapper>
+            <TextInput value={delegateAddressToAdd} onInput={onInput} placeholder="Enter delegate address" />
+            <Button variant="primary" label="Add delegate wallet" onClick={() => {}} height={45} width="100%" />
+          </AddDelegateInputWrapper>
+        )}
+        {showPendingRequests &&
+          pendingRequests.map(({ delegate, transactionHash }) => (
+            <PendingRequestWrapper key={transactionHash}>
+              <AddressWrapper>
+                <PendingRequestIcon />
+                <PendingRequestDetailsWrapper>
+                  <PendingRequestText>Request sent to {delegate}</PendingRequestText>
+                  <PendingRequestText>Waiting for approval</PendingRequestText>
+                  <PendingRequestText>
+                    <NextLink href={`https://goerli.etherscan.io/${transactionHash}`} passHref>
+                      <A target="_blank">View Transaction</A>
+                    </NextLink>
+                  </PendingRequestText>
+                </PendingRequestDetailsWrapper>
+              </AddressWrapper>
+            </PendingRequestWrapper>
+          ))}
         <PanelErrorBanner errorType="delegation" />
       </InnerWrapper>
       <PanelFooter />
@@ -69,10 +104,32 @@ export function DelegationPanel() {
   );
 }
 
-const WalletInputWrapper = styled.div`
+const PendingRequestText = styled(PanelSectionText)`
+  margin: 0;
+`;
+
+const PendingRequestDetailsWrapper = styled.div`
+  > :last-child {
+    margin-top: 5px;
+  }
+`;
+
+const AddDelegateInputWrapper = styled.div`
   margin-top: 30px;
   display: grid;
   gap: 20px;
+`;
+
+const PendingRequestWrapper = styled.div`
+  margin-top: 30px;
+  padding-block: 10px;
+  padding-inline: 12px;
+  background: var(--grey-100);
+  border-radius: 2px;
+`;
+
+const PendingRequestIcon = styled(Time)`
+  margin-top: 2px;
 `;
 
 const InnerWrapper = styled.div`
@@ -93,29 +150,37 @@ const StepWrapper = styled.li`
   font: var(--text-sm);
 `;
 
-const OneIcon = styled(One)`
+const numberIconStyle = css`
   circle {
     fill: var(--black);
   }
   path {
     fill: var(--white);
   }
+`;
+
+const OneIcon = styled(One)`
+  ${numberIconStyle}
 `;
 
 const TwoIcon = styled(Two)`
-  circle {
-    fill: var(--black);
-  }
-  path {
-    fill: var(--white);
-  }
+  ${numberIconStyle}
 `;
 
 const ThreeIcon = styled(Three)`
-  circle {
-    fill: var(--black);
-  }
-  path {
-    fill: var(--white);
+  ${numberIconStyle}
+`;
+
+export const AddressWrapper = styled.div`
+  display: flex;
+  align-items: top;
+  gap: 15px;
+`;
+
+const A = styled.a`
+  color: var(--red-500);
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
   }
 `;
