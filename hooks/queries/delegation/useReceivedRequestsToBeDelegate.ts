8b0d8710421c9f0aefc9745569369020e03a3e42
ryanwolhuter <dev@ryanwolhuter.com>
2022-10-17 10:39:41 +0200

    move ignored filtering logic to query hook
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/queries/delegation/useReceivedRequestsToBeDelegate.ts
+++ hooks/queries/delegation/useReceivedRequestsToBeDelegate.ts
@@ -1,16 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { receivedRequestsToBeDelegateKey } from "constants/queryKeys";
-import { useContractsContext, useHandleError, useUserContext } from "hooks";
+import { useContractsContext, useHandleError, useIgnoredRequestToBeDelegateAddresses, useUserContext } from "hooks";
 import { getDelegateSetEvents } from "web3";
 
 export function useReceivedRequestsToBeDelegate() {
   const { voting } = useContractsContext();
   const { address } = useUserContext();
   const onError = useHandleError();
+  const { data: ignoredRequestToBeDelegateAddresses } = useIgnoredRequestToBeDelegateAddresses();
 
   const queryResult = useQuery(
     [receivedRequestsToBeDelegateKey, address],
-    () => getDelegateSetEvents(voting, address, "delegate"),
+    () =>
+      getDelegateSetEvents(voting, address, "delegate").then((events) =>
+        events.filter((event) => !ignoredRequestToBeDelegateAddresses?.includes(event.delegator))
+      ),
     {
       refetchInterval: (data) => (data ? false : 100),
       initialData: [],
--- web3/queries/delegation/getDelegateSetEvents.ts
+++ web3/queries/delegation/getDelegateSetEvents.ts
@@ -1,6 +1,5 @@
 import { VotingV2Ethers } from "@uma/contracts-frontend";
 import { onlyOneRequestPerAddress } from "helpers";
-import { getIgnoredRequestToBeDelegateAddresses } from "./getIgnoredRequestToBeDelegateAddresses";
 
 export async function getDelegateSetEvents(
   voting: VotingV2Ethers,
@@ -10,14 +9,11 @@ export async function getDelegateSetEvents(
   const args = queryFor === "delegate" ? [null, address] : [address, null];
   const filter = voting.filters.DelegateSet(...args);
   const events = await voting.queryFilter(filter);
-  const ignoredDelegateSetEvents = await getIgnoredRequestToBeDelegateAddresses(address);
-  const parsedEvents = events
-    .map((event) => ({
-      delegate: event.args.delegate,
-      delegator: event.args.delegator,
-      transactionHash: event.transactionHash,
-    }))
-    .filter(({ delegator }) => !ignoredDelegateSetEvents.includes(delegator));
+  const parsedEvents = events.map((event) => ({
+    delegate: event.args.delegate,
+    delegator: event.args.delegator,
+    transactionHash: event.transactionHash,
+  }));
 
   return onlyOneRequestPerAddress(parsedEvents, queryFor);
 }
