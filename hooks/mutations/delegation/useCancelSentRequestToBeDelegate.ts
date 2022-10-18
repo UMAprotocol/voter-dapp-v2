50d7329dc0266982aabda6131d3f1f6ce5cf05c3
ryanwolhuter <dev@ryanwolhuter.com>
2022-10-18 13:15:13 +0200

    fix wrong remove function
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/mutations/delegation/useCancelSentRequestToBeDelegate.ts
+++ hooks/mutations/delegation/useCancelSentRequestToBeDelegate.ts
@@ -2,14 +2,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { sentRequestsToBeDelegateKey } from "constants/queryKeys";
 import { useHandleError, useUserContext } from "hooks";
 import { DelegationEventT } from "types";
-import { removeDelegator } from "web3";
+import { removeDelegate } from "web3";
 
 export function useCancelSentRequestToBeDelegate() {
   const { address } = useUserContext();
   const onError = useHandleError();
   const queryClient = useQueryClient();
 
-  const { mutate, isLoading } = useMutation(removeDelegator, {
+  const { mutate, isLoading } = useMutation(removeDelegate, {
     onError,
     onSuccess: () => {
       queryClient.setQueryData<DelegationEventT[]>([sentRequestsToBeDelegateKey, address], () => {
