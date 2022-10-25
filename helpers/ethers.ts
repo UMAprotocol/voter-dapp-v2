73b8c4cf059ac157376afd9d5892c0a092daaf3d
David A <4429761+daywiss@users.noreply.github.com>
2022-10-25 08:38:10 -0400

    fix: catch ether parsing errors from user input (#46)

--- helpers/ethers.ts
+++ helpers/ethers.ts
@@ -4,6 +4,15 @@ export const formatEther = ethers.utils.formatEther;
 
 export const parseEther = ethers.utils.parseEther;
 
+// This catches any potential errors form parsing an unknown string value, returns 0 if error happens.
+export function parseEtherSafe(value: string, decimals = 18): ethers.BigNumber {
+  try {
+    return ethers.utils.parseUnits(Number(value).toFixed(decimals), decimals);
+  } catch (err) {
+    return ethers.BigNumber.from(0);
+  }
+}
+
 export const solidityKeccak256 = ethers.utils.solidityKeccak256;
 
 export const randomBytes = ethers.utils.randomBytes;
