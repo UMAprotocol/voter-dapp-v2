83ea89accd37e95ecf0173564e5178753ede44f2
ryanwolhuter <dev@ryanwolhuter.com>
2022-10-25 16:32:55 +0200

    handle dangling delegate
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/delegation/getDelegateSetEvents.ts
+++ web3/queries/delegation/getDelegateSetEvents.ts
@@ -1,5 +1,6 @@
 import { VotingV2Ethers } from "@uma/contracts-frontend";
-import { onlyOneRequestPerAddress } from "helpers";
+import { onlyOneRequestPerAddress, zeroAddress } from "helpers";
+import { getStakerDetails } from "../balances/getStakerDetails";
 
 export async function getDelegateSetEvents(
   voting: VotingV2Ethers,
@@ -9,11 +10,36 @@ export async function getDelegateSetEvents(
   const args = queryFor === "delegate" ? [null, address] : [address, null];
   const filter = voting.filters.DelegateSet(...args);
   const events = await voting.queryFilter(filter);
-  const parsedEvents = events.map((event) => ({
-    delegate: event.args.delegate,
-    delegator: event.args.delegator,
-    transactionHash: event.transactionHash,
-  }));
+  const parsedEvents = events
+    .map((event) => ({
+      delegate: event.args.delegate,
+      delegator: event.args.delegator,
+      transactionHash: event.transactionHash,
+    }))
+    .filter(({ delegate }) => delegate !== zeroAddress);
 
-  return onlyOneRequestPerAddress(parsedEvents, queryFor);
+  const onlyOne = onlyOneRequestPerAddress(parsedEvents, queryFor);
+  return removeDanglingDelegateEvents(voting, onlyOne);
+}
+
+async function removeDanglingDelegateEvents(
+  voting: VotingV2Ethers,
+  events: {
+    delegate: string;
+    delegator: string;
+    transactionHash: string;
+  }[]
+) {
+  const result = [];
+  for (const event of events) {
+    const stillWantToBeDelegator = await detectDanglingDelegate(voting, event.delegator);
+    if (stillWantToBeDelegator) result.push(event);
+  }
+  return result;
+}
+
+async function detectDanglingDelegate(voting: VotingV2Ethers, delegator: string) {
+  const { delegate } = await getStakerDetails(voting, delegator);
+  if (delegate === zeroAddress) return false;
+  return true;
 }
