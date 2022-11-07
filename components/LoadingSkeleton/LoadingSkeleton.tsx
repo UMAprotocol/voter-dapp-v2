b28fd771a658e073120de89a8976b8b9aa558950
ryanwolhuter <dev@ryanwolhuter.com>
2022-11-07 13:39:32 +0200

    use children to determine loader height
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/LoadingSkeleton/LoadingSkeleton.tsx
+++ components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -1,30 +1,41 @@
 import { loadingSkeletonOpacity100, white, whiteOpacity10 } from "constant";
+import { cloneElement } from "react";
 import styled, { CSSProperties, keyframes } from "styled-components";
 
 interface Props {
-  width?: string | number;
-  height?: string | number;
+  children: JSX.Element;
+  isLoading: boolean;
+  height?: number;
+  width?: number;
   variant?: "grey" | "white";
 }
 export function LoadingSkeleton({
-  width = 30,
-  height = 16,
+  children,
+  isLoading,
+  width,
+  height,
   variant = "grey",
 }: Props) {
-  if (typeof width === "number") width = `${width}px`;
-  if (typeof height === "number") height = `${height}px`;
   const opaqueColor = variant === "grey" ? loadingSkeletonOpacity100 : white;
   const semiTransparentColor =
     variant === "grey" ? loadingSkeletonOpacity100 : whiteOpacity10;
 
   const style = {
-    "--width": width,
-    "--height": height,
     "--opaque-color": opaqueColor,
     "--semi-transparent-color": semiTransparentColor,
+    "--width": width ? `${width}px` : undefined,
+    "--height": height ? `${height}px` : undefined,
   } as CSSProperties;
 
-  return <Wrapper style={style}></Wrapper>;
+  const placeholder = cloneElement(children, {
+    children: <Placeholder>0</Placeholder>,
+  });
+
+  return isLoading ? (
+    <Wrapper style={style}>{placeholder}</Wrapper>
+  ) : (
+    <>{children}</>
+  );
 }
 
 const shine = keyframes`
@@ -39,11 +50,15 @@ const shine = keyframes`
   }
 `;
 
+const Placeholder = styled.span`
+  display: inline-block;
+  opacity: 0;
+`;
+
 const Wrapper = styled.span`
   display: inline-block;
-  margin-bottom: -2px;
-  width: var(--width);
-  height: var(--height);
+  width: var(--width, 90%);
+  height: var(--height, 90%);
   background: linear-gradient(
     to right,
     var(--opaque-color),
