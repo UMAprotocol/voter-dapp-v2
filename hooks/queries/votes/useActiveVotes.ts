482e4bd4b2ed337ed7ed90b75eba57ad24e23bcc
ryanwolhuter <dev@ryanwolhuter.com>
2022-11-14 10:46:21 +0200

    add custom refetch for active votes
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/queries/votes/useActiveVotes.ts
+++ hooks/queries/votes/useActiveVotes.ts
@@ -3,6 +3,7 @@ import { activeVotesKey } from "constant";
 import {
   useContractsContext,
   useHandleError,
+  useVotesContext,
   useVoteTimingContext,
 } from "hooks";
 import { getActiveVotes } from "web3";
@@ -10,13 +11,22 @@ import { getActiveVotes } from "web3";
 export function useActiveVotes() {
   const { voting } = useContractsContext();
   const { roundId } = useVoteTimingContext();
+  const { hasActiveVotes, hasUpcomingVotes } = useVotesContext();
   const { onError } = useHandleError({ isDataFetching: true });
 
   const queryResult = useQuery(
-    [activeVotesKey, roundId],
+    [activeVotesKey, roundId, hasActiveVotes],
     () => getActiveVotes(voting),
     {
-      refetchInterval: (data) => (data ? false : 100),
+      refetchInterval: (data) => {
+        if (data === undefined) return 100;
+
+        if (hasActiveVotes && Object.keys(data).length === 0) {
+          return 100;
+        }
+
+        return false;
+      },
       initialData: {},
       onError,
     }
--- hooks/queries/votes/useHasActiveVotes.ts
+++ hooks/queries/votes/useHasActiveVotes.ts
@@ -3,6 +3,7 @@ import { hasActiveVotesKey } from "constant";
 import {
   useContractsContext,
   useHandleError,
+  useVotesContext,
   useVoteTimingContext,
 } from "hooks";
 import { getHasActiveVotes } from "web3";
@@ -10,13 +11,18 @@ import { getHasActiveVotes } from "web3";
 export function useHasActiveVotes() {
   const { voting } = useContractsContext();
   const { roundId } = useVoteTimingContext();
+  const { hasUpcomingVotes } = useVotesContext();
   const { onError } = useHandleError({ isDataFetching: true });
 
   const queryResult = useQuery(
     [hasActiveVotesKey, roundId],
     () => getHasActiveVotes(voting),
     {
-      refetchInterval: (data) => (data ? false : 100),
+      refetchInterval: (data) => {
+        if (data === undefined) return 100;
+        if (hasUpcomingVotes && !data) return 100;
+        return false;
+      },
       onError,
     }
   );
