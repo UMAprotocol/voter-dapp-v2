0e8d421915a81cede80fa50a4f7e5fc2d46e0129
ryanwolhuter <dev@ryanwolhuter.com>
2022-11-17 09:51:24 +0200

    add mocks to vote page story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/Pages/VotePage/VotePage.stories.tsx
+++ stories/Pages/VotePage/VotePage.stories.tsx
@@ -1,11 +1,99 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
+import {
+  defaultVotesContextState,
+  defaultVoteTimingContextState,
+  VotesContext,
+  VoteTimingContext,
+} from "contexts";
 import VotePage from "pages";
+import {
+  voteCommitted,
+  voteCommittedButNotRevealed,
+  voteRevealed,
+  voteWithCorrectVoteWithoutUserVote,
+  voteWithCorrectVoteWithUserVote,
+  voteWithoutUserVote,
+} from "stories/mocks/votes";
+import { ActivityStatusT, VoteT } from "types";
+
+interface StoryProps {
+  phase: "commit" | "reveal";
+  activeVotes: VoteT[];
+  upcomingVotes: VoteT[];
+  pastVotes: VoteT[];
+  activityStatus: ActivityStatusT;
+}
 
 export default {
   title: "Pages/Vote Page/VotePage",
   component: VotePage,
-} as ComponentMeta<typeof VotePage>;
+} as Meta<StoryProps>;
 
-const Template: ComponentStory<typeof VotePage> = () => <VotePage />;
+const Template: Story<StoryProps> = (args) => {
+  const mockVoteTimingContextState = {
+    ...defaultVoteTimingContextState,
+    phase: args.phase ?? "commit",
+    roundId: 1,
+  };
 
-export const Default = Template.bind({});
+  const mockVotesContextState = {
+    ...defaultVotesContextState,
+    getActiveVotes: () => args.activeVotes ?? [],
+    getUpcomingVotes: () => args.upcomingVotes ?? [],
+    getPastVotes: () => args.pastVotes ?? [],
+    getActivityStatus: () => args.activityStatus ?? "past",
+  };
+
+  return (
+    <VoteTimingContext.Provider value={mockVoteTimingContextState}>
+      <VotesContext.Provider value={mockVotesContextState}>
+        <VotePage />
+      </VotesContext.Provider>
+    </VoteTimingContext.Provider>
+  );
+};
+
+export const ActiveCommit = Template.bind({});
+ActiveCommit.args = {
+  activityStatus: "active",
+  phase: "commit",
+  activeVotes: [
+    voteWithoutUserVote,
+    voteCommitted,
+    voteWithoutUserVote,
+    voteCommitted,
+  ],
+};
+
+export const ActiveReveal = Template.bind({});
+ActiveReveal.args = {
+  activityStatus: "active",
+  phase: "reveal",
+  activeVotes: [
+    voteCommittedButNotRevealed,
+    voteRevealed,
+    voteCommittedButNotRevealed,
+    voteRevealed,
+  ],
+};
+
+export const Upcoming = Template.bind({});
+Upcoming.args = {
+  activityStatus: "upcoming",
+  upcomingVotes: [
+    voteWithoutUserVote,
+    voteWithoutUserVote,
+    voteWithoutUserVote,
+  ],
+};
+
+export const Past = Template.bind({});
+Past.args = {
+  activityStatus: "past",
+  pastVotes: [
+    voteWithCorrectVoteWithUserVote,
+    voteWithCorrectVoteWithoutUserVote,
+    voteWithCorrectVoteWithUserVote,
+    voteWithCorrectVoteWithoutUserVote,
+  ],
+};
