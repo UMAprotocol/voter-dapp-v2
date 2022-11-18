85390f9fb981f1b09e5878d7550ec47de7e3eb86
ryanwolhuter <dev@ryanwolhuter.com>
2022-11-18 09:48:54 +0200

    add decoded admin transaction to vote mock
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/mocks/votes.ts
+++ stories/mocks/votes.ts
@@ -20,6 +20,7 @@ export const voteWithoutUserVote = {
   ancillaryData: "0x1234567890",
   decodedIdentifier: "SuperUMAn DAO KPI Options funding proposal",
   decodedAncillaryData: "Test test test",
+  decodedAdminTransaction: undefined,
   uniqueKey: "0x1234567890",
   umipNumber: 20,
   encryptedVote: undefined,
