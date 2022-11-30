82787ca99fd3718ad61e933d2811b4f9da8608da
ryanwolhuter <dev@ryanwolhuter.com>
2022-11-30 13:38:41 +0200

    get encrypted votes for all rounds including v1
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/queries/votes/useEncryptedVotes.ts
+++ hooks/queries/votes/useEncryptedVotes.ts
@@ -9,14 +9,14 @@ import {
 import { getEncryptedVotes } from "web3";
 
 export function useEncryptedVotes() {
-  const { voting } = useContractsContext();
+  const { voting, votingV1 } = useContractsContext();
   const { address } = useAccountDetails();
   const { roundId } = useVoteTimingContext();
   const { onError } = useHandleError({ isDataFetching: true });
 
   const queryResult = useQuery(
     [encryptedVotesKey, address, roundId],
-    () => getEncryptedVotes(voting, address, roundId),
+    () => getEncryptedVotes(voting, votingV1, address),
     {
       enabled: !!address,
       initialData: {},
