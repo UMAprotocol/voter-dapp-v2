36173cf9227dac66e9154b6b84a1804778842b6a
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-05 09:04:51 +0200

    add mobile styles
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/MigrationBanner/MigrationBanner.tsx
+++ components/MigrationBanner/MigrationBanner.tsx
@@ -16,43 +16,58 @@ export function MigrationBanner() {
   if (!show) return null;
 
   return (
-    <Wrapper>
-      <LogoIcon />
-      <Text>
-        Welcome to UMA 2.0 voting app.{" "}
-        <Link href="https://todo.com" target="_blank">
-          Read our staking & migration instructions here
-        </Link>
-      </Text>
-      <TabletText>
-        Welcome to UMA 2.0.{" "}
-        <Link href="https://todo.com" target="_blank">
-          Read the migrate and stake instructions
-        </Link>
-      </TabletText>
-      <MobileText>
-        Welcome!{" "}
-        <Link href="https://todo.com" target="_blank">
-          Learn to migrate and stake
-        </Link>
-      </MobileText>
+    <OuterWrapper>
+      <InnerWrapper>
+        <LogoIcon />
+        <Text>
+          Welcome to UMA 2.0 voting app. Read our{" "}
+          <Link href="https://todo.com" target="_blank">
+            staking & migration instructions here
+          </Link>
+        </Text>
+        <TabletText>
+          Welcome to UMA 2.0. Read the{" "}
+          <Link href="https://todo.com" target="_blank">
+            migrate and stake instructions
+          </Link>
+        </TabletText>
+        <MobileText>
+          Welcome! Learn to{" "}
+          <Link href="https://todo.com" target="_blank">
+            migrate and stake
+          </Link>
+        </MobileText>
 
-      <CloseButton onClick={close}>
-        <IconWrapper>
-          <CloseIcon />
-        </IconWrapper>
-      </CloseButton>
-    </Wrapper>
+        <CloseButton onClick={close}>
+          <IconWrapper>
+            <CloseIcon />
+          </IconWrapper>
+        </CloseButton>
+      </InnerWrapper>
+    </OuterWrapper>
   );
 }
 
-const Wrapper = styled.div`
+const OuterWrapper = styled.div`
   --height: 60px;
-  background: var(--black);
   height: var(--height);
+  background: var(--black);
+  display: grid;
+  align-items: center;
+`;
+
+const InnerWrapper = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
+  width: 100%;
+  max-width: var(--page-width);
   padding-inline: var(--page-padding);
+  margin-inline: auto;
+
+  @media ${tabletAndUnder} {
+    padding-inline: 0;
+  }
 `;
 
 const LogoIcon = styled(Logo)`
@@ -99,10 +114,16 @@ const CloseIcon = styled(Close)`
 `;
 const CloseButton = styled.button`
   --icon-height: 15px;
-  --icon-offset: 10px;
+  --icon-offset: 12px;
+  --icon-start-right: var(--page-padding);
+  --right: calc(var(--icon-start-right) + var(--icon-offset));
   position: absolute;
   top: calc(var(--height) - var(--icon-height) / 2px);
-  right: calc(var(--page-padding) + var(--icon-offset));
+  right: var(--right);
   fill: var(--white);
   background: transparent;
+
+  @media ${tabletAndUnder} {
+    --icon-start-right: 0px;
+  }
 `;
