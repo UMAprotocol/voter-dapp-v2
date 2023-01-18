89ec494e49eb9c5eee76ec78426551f04ba01b3c
david <david@umaproject.org>
2023-01-18 08:42:43 -0500

    fix: use getPendingRequestsPostUpdate
    Signed-off-by: david <david@umaproject.org>

--- web3/queries/votes/getActiveVotes.ts
+++ web3/queries/votes/getActiveVotes.ts
@@ -2,7 +2,8 @@ import { VotingV2Ethers } from "@uma/contracts-frontend";
 import { makePriceRequestsByKey } from "helpers";
 
 export async function getActiveVotes(votingContract: VotingV2Ethers) {
-  const pendingRequests = await votingContract.callStatic.getPendingRequests();
+  const pendingRequests =
+    await votingContract.callStatic.getPendingRequestsPostUpdate();
   const activeVotes = makePriceRequestsByKey(pendingRequests);
   const hasActiveVotes = Object.keys(activeVotes).length > 0;
   return {
