8524d660564d4b9e98ddf6db6a99acdf4cd6527b
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-02 12:05:59 +0200

    use const for discord thread query key
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- hooks/queries/discord/useDiscordThread.ts
+++ hooks/queries/discord/useDiscordThread.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getDiscordThread } from "web3";
 import { L1Request } from "types";
+import { discordThreadKey } from "constant";
 
 export function useDiscordThread(params: L1Request) {
   return useQuery({
-    queryKey: ["discordThread", params.identifier, params.time],
+    queryKey: [discordThreadKey, params.identifier, params.time],
     queryFn: () => getDiscordThread(params),
     onError: (err) => console.error(err),
   });
