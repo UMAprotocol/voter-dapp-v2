ab3559772c820a70485cc5e927830e562d72ec67
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-02 15:11:52 +0200

    rename discussion type for clarity
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ hooks/queries/votes/useVoteDiscussion.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { voteDiscussionKey } from "constant";
+import { L1Request } from "types";
+import { getVoteDiscussion } from "web3";
+
+export function useVoteDiscussion({ identifier, time }: L1Request) {
+  return useQuery({
+    queryKey: [voteDiscussionKey, identifier, time],
+    queryFn: () => getVoteDiscussion({ identifier, time }),
+    onError: (err) => console.error(err),
+    initialData: {
+      identifier,
+      time,
+      thread: [],
+    },
+  });
+}
