13fca76ee1e31c5dd41370abca1ed3acf4c5b716
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-20 13:39:02 +0200

    update get pending requests method
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/votes/getActiveVotes.ts
+++ web3/queries/votes/getActiveVotes.ts
@@ -2,8 +2,7 @@ import { VotingV2Ethers } from "@uma/contracts-frontend";
 import { makePriceRequestsByKey } from "helpers";
 
 export async function getActiveVotes(votingContract: VotingV2Ethers) {
-  const pendingRequests =
-    await votingContract.callStatic.getPendingRequestsPostUpdate();
+  const pendingRequests = await votingContract.getPendingRequests();
   const activeVotes = makePriceRequestsByKey(pendingRequests);
   const hasActiveVotes = Object.keys(activeVotes).length > 0;
   return {
