d33f5a3d06cf2a93d489b4012df29e944d7bb43e
smb2796 <sean.brown2796@gmail.com>
2023-02-22 15:12:45 -0500

    lint fix

--- components/pages/WalletSettings/AddDelegate.tsx
+++ components/pages/WalletSettings/AddDelegate.tsx
@@ -11,10 +11,10 @@ export function AddDelegate() {
       <Text>
         A delegate is a wallet that has been chosen to vote on behalf of another
         party. If acting as a delegate, a delegate can no longer vote for
-        themselves. Delegates can commit and reveal votes on behalf of a delegator
-        as well as claim and stake reward tokens. A delegate cannot however
-        unstake tokens for a delegator. A delegate can only be a delegate for a single
-        delegator.
+        themselves. Delegates can commit and reveal votes on behalf of a
+        delegator as well as claim and stake reward tokens. A delegate cannot
+        however unstake tokens for a delegator. A delegate can only be a
+        delegate for a single delegator.
       </Text>
       <BarWrapper>
         <BarText>No delegate wallet selected</BarText>
