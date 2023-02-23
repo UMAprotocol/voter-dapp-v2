9cc107b9b3c3c516b4b68ecf6894ec06a7947f45
david <david@umaproject.org>
2023-02-23 07:41:58 -0500

    fix: copy around connected wallet delegate
    Signed-off-by: david <david@umaproject.org>

--- components/pages/WalletSettings/ConnectedWallet.tsx
+++ components/pages/WalletSettings/ConnectedWallet.tsx
@@ -23,10 +23,23 @@ export function ConnectedWallet({ status }: Props) {
         Connected Wallet {status === "delegate" && "(Delegate)"}
         {status === "delegator" && "(Delegator)"}
       </Header>
-      <Text>
-        A delegator is a wallet that has chosen to delegate its voting power to
-        another party. Delegators can only delegate to one address at a time.
-      </Text>
+      {status === "delegator" && (
+        <Text>
+          A delegator is a wallet that has chosen to delegate its voting power
+          to another party. Delegators can only delegate to one address at a
+          time.
+        </Text>
+      )}
+      {status === "delegate" && (
+        <Text>
+          A delegate is a wallet that has been chosen to vote on behalf of
+          another party. If acting as a delegate, a delegate can no longer vote
+          for themselves. Delegates can commit & reveal votes on behalf of a
+          delegator, as well as claim and stake reward tokens. A delegate cannot
+          unstake tokens for a delegator. A delegate can only be a delegate for
+          a single delegator.
+        </Text>
+      )}
       <BarWrapper>
         <WalletWrapper>
           <WalletIcon icon={walletIcon} />
