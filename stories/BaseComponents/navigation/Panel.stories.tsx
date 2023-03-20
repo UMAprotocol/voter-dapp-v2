aea308cf3e4e9efd6cb774460d1e6db8666c6c85
ryanwolhuter <dev@ryanwolhuter.com>
2023-03-20 16:05:58 +0200

    use new story syntax for panel story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/BaseComponents/navigation/Panel.stories.tsx
+++ stories/BaseComponents/navigation/Panel.stories.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-import { useArgs } from "@storybook/client-api";
-import { DecoratorFn, Meta, Story } from "@storybook/react";
+import { Decorator, Meta, StoryObj } from "@storybook/react";
 import { Button, Panel } from "components";
 import {
   defaultDelegationContextState,
@@ -22,6 +19,7 @@ import {
 } from "contexts";
 import { BigNumber } from "ethers";
 import { bigNumberFromFloatString, zeroAddress } from "helpers";
+import { useState } from "react";
 import {
   mockAddress1,
   mockAddress2,
@@ -31,12 +29,14 @@ import { mockWalletIcon } from "stories/mocks/mockWalletIcon";
 import { makeMockVotesWithHistory, voteCommitted } from "stories/mocks/votes";
 import { DelegationEventT, DelegationStatusT, VoteT } from "types";
 
-interface StoryProps
+interface Props
   extends PanelContextState,
     ErrorContextState,
     VotesContextState,
     UserContextState,
     DelegationContextState {
+  panelType: PanelContextState["panelType"];
+  panelContent: PanelContextState["panelContent"];
   votes: VoteT[];
   delegationStatus: DelegationStatusT;
   delegateAddress: string;
@@ -45,24 +45,31 @@ interface StoryProps
   pendingReceivedRequestsToBeDelegate: DelegationEventT[];
 }
 
-export default {
-  title: "Base components/Navigation/Panel",
+const meta: Meta<Props> = {
   component: Panel,
-} as Meta<StoryProps>;
+  title: "Base components/Navigation/Panel",
+};
 
-const Template: Story<StoryProps> = (args) => {
-  const [_args, updateArgs] = useArgs();
-  const openPanel = () => {
-    updateArgs({ ...args, panelOpen: true });
-  };
-  const closePanel = () => {
-    updateArgs({ ...args, panelOpen: false });
-  };
+export default meta;
+
+type Story = StoryObj<Props>;
+
+function Wrapper({ panelType, panelContent }: Props) {
+  const [panelOpen, setPanelOpen] = useState(true);
+
+  function openPanel() {
+    setPanelOpen(true);
+  }
+
+  function closePanel() {
+    setPanelOpen(false);
+  }
+
   const mockPanelContextState = {
     ...defaultPanelContextState,
-    panelType: args.panelType ?? defaultPanelContextState.panelType,
-    panelOpen: args.panelOpen ?? true,
-    panelContent: args.panelContent ?? defaultPanelContextState.panelContent,
+    panelType: panelType ?? defaultPanelContextState.panelType,
+    panelOpen: panelOpen ?? true,
+    panelContent: panelContent ?? defaultPanelContextState.panelContent,
     openPanel,
     closePanel,
   };
@@ -73,9 +80,13 @@ const Template: Story<StoryProps> = (args) => {
       <Panel />
     </PanelContext.Provider>
   );
+}
+
+const Template: Story = {
+  render: (args) => <Wrapper {...args} />,
 };
 
-const withErrorDecorator: DecoratorFn = (Story, { args }) => {
+const errorDecorator: Decorator<Props> = (Story, { args }) => {
   const mockErrorMessages = {
     claim: ["Something went wrong in claim panel"],
     stake: ["Something went wrong in stake panel"],
@@ -101,10 +112,10 @@ const mockConnectedWallet = {
   chains: [],
 };
 
-const userDecorator: DecoratorFn = (Story, { args }) => {
+const userDecorator: Decorator<Props> = (Story, { args }) => {
   const mockUserContextState: UserContextState = {
     ...defaultUserContextState,
-    apr: args.apr ?? 0,
+    apr: args.apr ?? BigNumber.from(0),
     cumulativeCalculatedSlash:
       args.cumulativeCalculatedSlash ?? BigNumber.from(0),
     cumulativeCalculatedSlashPercentage:
@@ -122,7 +133,7 @@ const userDecorator: DecoratorFn = (Story, { args }) => {
   );
 };
 
-const votesDecorator: DecoratorFn = (Story, { args }) => {
+const votesDecorator: Decorator<Props> = (Story, { args }) => {
   const mockVotesContextState: VotesContextState = {
     ...defaultVotesContextState,
     getPastVotes: () => args.votes ?? [],
@@ -135,7 +146,7 @@ const votesDecorator: DecoratorFn = (Story, { args }) => {
   );
 };
 
-const delegationDecorator: DecoratorFn = (Story, { args }) => {
+const delegationDecorator: Decorator<Props> = (Story, { args }) => {
   const mockDelegationContextState: DelegationContextState = {
     ...defaultDelegationContextState,
     getDelegationStatus: () => args.delegationStatus ?? "no-delegation",
@@ -154,252 +165,301 @@ const delegationDecorator: DecoratorFn = (Story, { args }) => {
   );
 };
 
-export const MenuPanelNoWalletConnected = Template.bind({});
-MenuPanelNoWalletConnected.args = {
-  panelType: "menu",
-  panelOpen: true,
-  address: "",
-  connectedWallet: undefined,
-  walletIcon: undefined,
-  delegationStatus: "no-wallet-connected",
-};
-MenuPanelNoWalletConnected.decorators = [withErrorDecorator, userDecorator];
-
-export const MenuPanelNoDelegation = Template.bind({});
-MenuPanelNoDelegation.args = {
-  panelType: "menu",
-  panelOpen: true,
-  address: mockAddress1,
-  // @ts-expect-error - no need to do a complicated mock for the provider here
-  connectedWallet: mockConnectedWallet,
-  delegationStatus: "no-delegation",
-};
-MenuPanelNoDelegation.decorators = [userDecorator, delegationDecorator];
-
-export const MenuPanelDelegator = Template.bind({});
-MenuPanelDelegator.args = {
-  panelType: "menu",
-  panelOpen: true,
-  address: mockAddress1,
-  // @ts-expect-error - no need to do a complicated mock for the provider here
-  connectedWallet: mockConnectedWallet,
-  delegationStatus: "delegator",
-  delegateAddress: mockAddress2,
-};
-MenuPanelDelegator.decorators = [userDecorator, delegationDecorator];
-
-export const MenuPanelDelegate = Template.bind({});
-MenuPanelDelegate.args = {
-  panelType: "menu",
-  panelOpen: true,
-  address: mockAddress2,
-  // @ts-expect-error - no need to do a complicated mock for the provider here
-  connectedWallet: mockConnectedWallet,
-  delegationStatus: "delegate",
-  delegatorAddress: mockAddress1,
-};
-MenuPanelDelegate.decorators = [userDecorator, delegationDecorator];
-
-export const MenuPanelDelegatorPending = Template.bind({});
-MenuPanelDelegatorPending.args = {
-  panelType: "menu",
-  panelOpen: true,
-  address: mockAddress1,
-  // @ts-expect-error - no need to do a complicated mock for the provider here
-  connectedWallet: mockConnectedWallet,
-  delegationStatus: "delegator-pending",
-  pendingSentRequestsToBeDelegate: [
-    {
-      delegator: mockAddress1,
-      delegate: mockAddress2,
-      transactionHash: mockDelegateRequestTransaction,
-    },
-  ],
-};
-MenuPanelDelegatorPending.decorators = [userDecorator, delegationDecorator];
-
-export const MenuPanelDelegatePending = Template.bind({});
-MenuPanelDelegatePending.args = {
-  panelType: "menu",
-  panelOpen: true,
-  address: mockAddress2,
-  // @ts-expect-error - no need to do a complicated mock for the provider here
-  connectedWallet: mockConnectedWallet,
-  delegationStatus: "delegate-pending",
-  pendingReceivedRequestsToBeDelegate: [
-    {
-      delegator: mockAddress1,
-      delegate: mockAddress2,
-      transactionHash: mockDelegateRequestTransaction,
-    },
-  ],
-};
-MenuPanelDelegatePending.decorators = [userDecorator, delegationDecorator];
-
-export const ClaimPanel = Template.bind({});
-ClaimPanel.args = {
-  panelType: "claim",
-  panelOpen: true,
+export const MenuPanelNoWalletConnected: Story = {
+  ...Template,
+  args: {
+    panelType: "menu",
+    panelOpen: true,
+    address: "",
+    connectedWallet: undefined,
+    walletIcon: undefined,
+    delegationStatus: "no-wallet-connected",
+  },
+  decorators: [errorDecorator, userDecorator],
 };
 
-export const ClaimPanelWithError = Template.bind({});
-ClaimPanelWithError.args = {
-  ...ClaimPanel.args,
+export const MenuPanelNoDelegation: Story = {
+  ...Template,
+  args: {
+    panelType: "menu",
+    panelOpen: true,
+    address: mockAddress1,
+    // @ts-expect-error - no need to do a complicated mock for the provider here
+    connectedWallet: mockConnectedWallet,
+    delegationStatus: "no-delegation",
+  },
+  decorators: [userDecorator, delegationDecorator],
 };
-ClaimPanelWithError.decorators = [withErrorDecorator];
 
-export const VotePanelWithoutResults = Template.bind({});
-VotePanelWithoutResults.args = {
-  panelType: "vote",
-  panelContent: voteCommitted,
-  panelOpen: true,
+export const MenuPanelDelegator: Story = {
+  ...Template,
+  args: {
+    panelType: "menu",
+    panelOpen: true,
+    address: mockAddress1,
+    // @ts-expect-error - no need to do a complicated mock for the provider here
+    connectedWallet: mockConnectedWallet,
+    delegationStatus: "delegator",
+    delegateAddress: mockAddress2,
+  },
+  decorators: [userDecorator, delegationDecorator],
 };
 
-export const VotePanelWithResults = Template.bind({});
-VotePanelWithResults.args = {
-  ...VotePanelWithoutResults.args,
-  panelContent: {
-    ...voteCommitted,
-    participation: {
-      uniqueCommitAddresses: 100,
-      uniqueRevealAddresses: 100,
-      totalTokensVotedWith: 800000000.123,
-    },
-    results: [
+export const MenuPanelDelegate: Story = {
+  ...Template,
+  args: {
+    panelType: "menu",
+    panelOpen: true,
+    address: mockAddress2,
+    // @ts-expect-error - no need to do a complicated mock for the provider here
+    connectedWallet: mockConnectedWallet,
+    delegationStatus: "delegate",
+    delegatorAddress: mockAddress1,
+  },
+  decorators: [userDecorator, delegationDecorator],
+};
+
+export const MenuPanelDelegatorPending: Story = {
+  ...Template,
+  args: {
+    panelType: "menu",
+    panelOpen: true,
+    address: mockAddress1,
+    // @ts-expect-error - no need to do a complicated mock for the provider here
+    connectedWallet: mockConnectedWallet,
+    delegationStatus: "delegator-pending",
+    pendingSentRequestsToBeDelegate: [
       {
-        vote: "50000000000",
-        tokensVotedWith: 1234,
-      },
-      {
-        vote: "20",
-        tokensVotedWith: 5678,
-      },
-      {
-        vote: "10",
-        tokensVotedWith: 500,
-      },
-      {
-        vote: "2",
-        tokensVotedWith: 199,
+        delegator: mockAddress1,
+        delegate: mockAddress2,
+        transactionHash: mockDelegateRequestTransaction,
       },
     ],
   },
+  decorators: [userDecorator, delegationDecorator],
 };
 
-export const VotePanelWithLongTitle = Template.bind({});
-VotePanelWithLongTitle.args = {
-  ...VotePanelWithoutResults.args,
-  panelContent: {
-    ...voteCommitted,
-    title:
-      "Will Coinbase support Polygon USDC deposits & withdrawals by June 30, 2022?",
+export const MenuPanelDelegatePending: Story = {
+  ...Template,
+  args: {
+    panelType: "menu",
+    panelOpen: true,
+    address: mockAddress2,
+    // @ts-expect-error - no need to do a complicated mock for the provider here
+    connectedWallet: mockConnectedWallet,
+    delegationStatus: "delegate-pending",
+    pendingReceivedRequestsToBeDelegate: [
+      {
+        delegator: mockAddress1,
+        delegate: mockAddress2,
+        transactionHash: mockDelegateRequestTransaction,
+      },
+    ],
+  },
+  decorators: [userDecorator, delegationDecorator],
+};
+
+export const ClaimPanel: Story = {
+  ...Template,
+  args: {
+    panelType: "claim",
+    panelOpen: true,
   },
 };
 
-export const VotePanelWithoutResultsWithError = Template.bind({});
-VotePanelWithoutResultsWithError.args = {
-  ...VotePanelWithoutResults.args,
-};
-VotePanelWithoutResultsWithError.decorators = [withErrorDecorator];
-
-export const VotePanelWithResultsWithError = Template.bind({});
-VotePanelWithResultsWithError.args = {
-  ...VotePanelWithResults.args,
-};
-VotePanelWithResultsWithError.decorators = [withErrorDecorator];
-
-export const StakePanel = Template.bind({});
-StakePanel.args = {
-  panelType: "stake",
-  panelOpen: true,
+export const ClaimPanelWithError: Story = {
+  ...Template,
+  args: {
+    ...ClaimPanel.args,
+  },
+  decorators: [errorDecorator],
 };
 
-export const StakePanelWithError = Template.bind({});
-StakePanelWithError.args = {
-  ...StakePanel.args,
-};
-StakePanelWithError.decorators = [withErrorDecorator];
-
-export const RemindPanel = Template.bind({});
-RemindPanel.args = {
-  panelType: "remind",
-  panelOpen: true,
+export const VotePanelWithoutResults: Story = {
+  ...Template,
+  args: {
+    panelType: "vote",
+    panelContent: voteCommitted,
+    panelOpen: true,
+  },
 };
 
-export const HistoryPanel = Template.bind({});
-HistoryPanel.decorators = [userDecorator, votesDecorator];
-HistoryPanel.args = {
-  panelType: "history",
-  apr: bigNumberFromFloatString(`${Math.random() * 100}`),
-  cumulativeCalculatedSlash: bigNumberFromFloatString(`${Math.random() * 100}`),
-  cumulativeCalculatedSlashPercentage: bigNumberFromFloatString(
-    `${Math.random() > 0.5 ? "-" : ""}${Math.random() * 100}`
-  ),
-  votes: makeMockVotesWithHistory(),
+export const VotePanelWithResults: Story = {
+  ...Template,
+  args: {
+    ...VotePanelWithoutResults.args,
+    panelContent: {
+      ...voteCommitted,
+      participation: {
+        uniqueCommitAddresses: 100,
+        uniqueRevealAddresses: 100,
+        totalTokensVotedWith: 800000000.123,
+      },
+      results: [
+        {
+          vote: "50000000000",
+          tokensVotedWith: 1234,
+        },
+        {
+          vote: "20",
+          tokensVotedWith: 5678,
+        },
+        {
+          vote: "10",
+          tokensVotedWith: 500,
+        },
+        {
+          vote: "2",
+          tokensVotedWith: 199,
+        },
+      ],
+    },
+  },
+};
+
+export const VotePanelWithLongTitle: Story = {
+  ...Template,
+  args: {
+    ...VotePanelWithoutResults.args,
+    panelContent: {
+      ...voteCommitted,
+      title:
+        "Will Coinbase support Polygon USDC deposits & withdrawals by June 30, 2022?",
+    },
+  },
+};
+
+export const VotePanelWithoutResultsWithError: Story = {
+  ...Template,
+  args: {
+    ...VotePanelWithoutResults.args,
+  },
+  decorators: [errorDecorator],
+};
+
+export const VotePanelWithResultsWithError: Story = {
+  ...Template,
+  args: {
+    ...VotePanelWithResults.args,
+  },
+  decorators: [errorDecorator],
+};
+
+export const StakePanel: Story = {
+  ...Template,
+  args: {
+    panelType: "stake",
+    panelOpen: true,
+  },
+};
+
+export const StakePanelWithError: Story = {
+  ...Template,
+  args: {
+    ...StakePanel.args,
+  },
+  decorators: [errorDecorator],
+};
+
+export const RemindPanel: Story = {
+  ...Template,
+  args: {
+    panelType: "remind",
+    panelOpen: true,
+  },
+};
+
+export const HistoryPanel: Story = {
+  ...Template,
+  args: {
+    panelType: "history",
+    apr: bigNumberFromFloatString(`${Math.random() * 100}`),
+    cumulativeCalculatedSlash: bigNumberFromFloatString(
+      `${Math.random() * 100}`
+    ),
+    cumulativeCalculatedSlashPercentage: bigNumberFromFloatString(
+      `${Math.random() > 0.5 ? "-" : ""}${Math.random() * 100}`
+    ),
+    votes: makeMockVotesWithHistory(),
+  },
+  decorators: [userDecorator, votesDecorator],
 };
 
 const delegationPanelDecorators = [userDecorator, delegationDecorator];
+
 const delegationPanelCommonArgs = {
   panelType: "delegation" as const,
   panelOpen: true,
 };
-export const DelegationPanel = Template.bind({});
-DelegationPanel.args = {
-  ...delegationPanelCommonArgs,
+export const DelegationPanel: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanel.decorators = delegationPanelDecorators;
 
 // this should render as nothing in the panel because we don't allow the user to open this panel when no wallet is connected
-export const DelegationPanelWhenNoWalletConnected = Template.bind({});
-DelegationPanelWhenNoWalletConnected.args = {
-  ...delegationPanelCommonArgs,
-  delegationStatus: "no-wallet-connected",
+export const DelegationPanelWhenNoWalletConnected: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+    delegationStatus: "no-wallet-connected",
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanelWhenNoWalletConnected.decorators = delegationPanelDecorators;
 
 // this should render nothing in the panel because we don't allow a delegate to add a delegate
-export const DelegationPanelWhenStatusIsDelegate = Template.bind({});
-DelegationPanelWhenStatusIsDelegate.args = {
-  ...delegationPanelCommonArgs,
-  delegationStatus: "delegate",
+export const DelegationPanelWhenStatusIsDelegate: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+    delegationStatus: "delegate",
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanelWhenStatusIsDelegate.decorators = delegationPanelDecorators;
 
 // this should render nothing in the panel because the user must ignore pending delegate requests before attempting to add their own delegate
-export const DelegationPanelWhenStatusIsDelegatePending = Template.bind({});
-DelegationPanelWhenStatusIsDelegatePending.args = {
-  ...delegationPanelCommonArgs,
-  delegationStatus: "delegate-pending",
+export const DelegationPanelWhenStatusIsDelegatePending: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+    delegationStatus: "delegate-pending",
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanelWhenStatusIsDelegatePending.decorators =
-  delegationPanelDecorators;
 
 // this should render nothing in the panel because we don't allow a delegator to add another delegate
-export const DelegationPanelWhenStatusIsDelegator = Template.bind({});
-DelegationPanelWhenStatusIsDelegator.args = {
-  ...delegationPanelCommonArgs,
-  delegationStatus: "delegator",
+export const DelegationPanelWhenStatusIsDelegator: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+    delegationStatus: "delegator",
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanelWhenStatusIsDelegator.decorators = delegationPanelDecorators;
 
-export const DelegationPanelWhenStatusIsNoDelegation = Template.bind({});
-DelegationPanelWhenStatusIsNoDelegation.args = {
-  ...delegationPanelCommonArgs,
-  delegationStatus: "no-delegation",
+export const DelegationPanelWhenStatusIsNoDelegation: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+    delegationStatus: "no-delegation",
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanelWhenStatusIsNoDelegation.decorators = delegationPanelDecorators;
 
-export const DelegationPanelWhenStatusIsDelegatorPending = Template.bind({});
-DelegationPanelWhenStatusIsDelegatorPending.args = {
-  ...delegationPanelCommonArgs,
-  delegationStatus: "delegator-pending",
-  pendingSentRequestsToBeDelegate: [
-    {
-      delegator: mockAddress1,
-      delegate: mockAddress2,
-      transactionHash: mockDelegateRequestTransaction,
-    },
-  ],
+export const DelegationPanelWhenStatusIsDelegatorPending: Story = {
+  ...Template,
+  args: {
+    ...delegationPanelCommonArgs,
+    delegationStatus: "delegator-pending",
+    pendingSentRequestsToBeDelegate: [
+      {
+        delegator: mockAddress1,
+        delegate: mockAddress2,
+        transactionHash: mockDelegateRequestTransaction,
+      },
+    ],
+  },
+  decorators: delegationPanelDecorators,
 };
-DelegationPanelWhenStatusIsDelegatorPending.decorators =
-  delegationPanelDecorators;
