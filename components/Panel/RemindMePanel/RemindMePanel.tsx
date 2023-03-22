e5995b39009b15accf8b937e5943a30bc28f7be3
ryanwolhuter <dev@ryanwolhuter.com>
2023-03-22 10:55:42 +0200

    add mailchimp handler to remind panel
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- components/Panel/RemindMePanel/RemindMePanel.tsx
+++ components/Panel/RemindMePanel/RemindMePanel.tsx
@@ -1,92 +1,83 @@
-import { Button, Checkbox, TextInput, Toggle } from "components";
+import { Button, Checkbox, PanelErrorBanner } from "components";
+import { EmailInput } from "components";
+import { mobileAndUnder } from "constant";
+import { config } from "helpers";
 import Check from "public/assets/icons/check.svg";
 import { FormEvent, useState } from "react";
 import styled from "styled-components";
+import { useMailChimpForm } from "use-mailchimp-form";
 import { PanelFooter } from "../PanelFooter";
 import { PanelTitle } from "../PanelTitle";
 import { PanelSectionText, PanelSectionTitle, PanelWrapper } from "../styles";
 
 export function RemindMePanel() {
   const [email, setEmail] = useState("");
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
   const [disclaimerAccepted, setDisclaimerAccepted] = useState(false);
-  const [notificationButtonClicked, setNotificationButtonClicked] =
-    useState(false);
+
+  const url = config.mailchimpUrl;
+
+  const { loading, success, message, handleSubmit } = useMailChimpForm(url);
 
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    setEmailSubmitted(true);
-    console.log(email, "TODO implement submit");
-    return false;
-  }
 
-  function onNotificationButtonClick() {
-    setNotificationButtonClicked(!notificationButtonClicked);
+    try {
+      handleSubmit({ EMAIL: email });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <PanelWrapper>
       <PanelTitle title="Remind me" />
       <SectionsWrapper>
-        {emailSubmitted ? (
-          <SectionWrapper>
-            <SuccessTitle>Success!</SuccessTitle>
-            <SuccessDescription>
-              Your email has been added TODO improve copy
-            </SuccessDescription>
-            <SuccessIconOuterWrapper>
-              <SuccessIconWrapper>
-                <SuccessIcon />
-              </SuccessIconWrapper>
-            </SuccessIconOuterWrapper>
-          </SectionWrapper>
-        ) : (
-          <SectionWrapper>
-            <PanelSectionTitle>Email reminder</PanelSectionTitle>
-            <PanelSectionText>
-              We’ll send out an email 24 hours before the voting commit and
-              reveal phases end.
-            </PanelSectionText>
-            <EmailForm onSubmit={onSubmit}>
-              <TextInput
-                placeholder="Your email"
-                type="email"
-                value={email}
-                onInput={setEmail}
-              />
-              <CheckboxWrapper>
-                <Checkbox
-                  label="I consent to receiving email notifications"
-                  checked={disclaimerAccepted}
-                  onChange={(e) => setDisclaimerAccepted(e.target.checked)}
-                  gap={5}
+        <InnerWrapper>
+          {success ? (
+            <SectionWrapper>
+              <SuccessTitle>Success!</SuccessTitle>
+              <SuccessDescription>{message}</SuccessDescription>
+              <SuccessIconOuterWrapper>
+                <SuccessIconWrapper>
+                  <SuccessIcon />
+                </SuccessIconWrapper>
+              </SuccessIconOuterWrapper>
+            </SectionWrapper>
+          ) : (
+            <SectionWrapper>
+              <PanelSectionTitle>Email reminder</PanelSectionTitle>
+              <PanelSectionText>
+                We&apos;ll send out an email 24 hours before the voting commit
+                and reveal phases end.
+              </PanelSectionText>
+              <EmailForm onSubmit={onSubmit}>
+                <EmailInput
+                  value={email}
+                  onInput={setEmail}
+                  disabled={loading}
                 />
-              </CheckboxWrapper>
-              <SubmitButtonWrapper>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  label="Submit"
-                  width="100%"
-                  disabled={!disclaimerAccepted}
-                />
-              </SubmitButtonWrapper>
-            </EmailForm>
-          </SectionWrapper>
-        )}
-        <BrowserReminderSectionWrapper>
-          <BrowserReminderTextWrapper>
-            <PanelSectionTitle>Browser reminder</PanelSectionTitle>
-            <PanelSectionText>
-              You’ll get notified in the browser 1 hour before the voting commit
-              and reveal phases end.
-            </PanelSectionText>
-          </BrowserReminderTextWrapper>
-          <Toggle
-            clicked={notificationButtonClicked}
-            onClick={onNotificationButtonClick}
-          />
-        </BrowserReminderSectionWrapper>
+                <CheckboxWrapper>
+                  <Checkbox
+                    label="I consent to receiving email notifications"
+                    checked={disclaimerAccepted}
+                    onChange={(e) => setDisclaimerAccepted(e.target.checked)}
+                    gap={5}
+                  />
+                </CheckboxWrapper>
+                <SubmitButtonWrapper>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    label="Submit"
+                    width="100%"
+                    disabled={!disclaimerAccepted || loading}
+                  />
+                </SubmitButtonWrapper>
+              </EmailForm>
+            </SectionWrapper>
+          )}
+          <PanelErrorBanner errorOrigin="remind" />
+        </InnerWrapper>
       </SectionsWrapper>
       <PanelFooter />
     </PanelWrapper>
@@ -100,8 +91,6 @@ const SectionWrapper = styled.div`
   padding-block: 20px;
   background: var(--grey-50);
   border-radius: 5px;
-  margin-block: 20px;
-  margin-inline: 25px;
 `;
 
 const SubmitButtonWrapper = styled.div`
@@ -112,20 +101,6 @@ const EmailForm = styled.form``;
 
 const CheckboxWrapper = styled.div`
   margin-block: 15px;
-`;
-
-const BrowserReminderSectionWrapper = styled(SectionWrapper)`
-  display: grid;
-  grid-template-columns: 70% 1fr;
-  align-items: end;
-  justify-items: end;
-  padding-bottom: 30px;
-`;
-
-const BrowserReminderTextWrapper = styled.div`
-  > ${PanelSectionText} {
-    margin-bottom: 0;
-  }
 `;
 
 const SuccessTitle = styled.h2`
@@ -159,5 +134,14 @@ const SuccessIcon = styled(Check)`
   path {
     fill: var(--white);
     stroke: var(--green);
+  }
+`;
+
+const InnerWrapper = styled.div`
+  padding-inline: 30px;
+  padding-block: 20px;
+
+  @media ${mobileAndUnder} {
+    padding-inline: 10px;
   }
 `;
