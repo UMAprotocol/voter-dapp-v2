93608eb3fad6eaa1b59a7d3d5cdaf7425a116dd0
David A <4429761+daywiss@users.noreply.github.com>
2023-03-22 13:13:42 -0400

    fix: merge revealed queries with existing cache (#235)
    * refactor hook to use onsuccess

--- hooks/queries/votes/useRevealedVotes.ts
+++ hooks/queries/votes/useRevealedVotes.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { revealedVotesKey } from "constant";
 import {
   useAccountDetails,
@@ -10,6 +10,7 @@ import {
 import { getRevealedVotes } from "web3";
 
 export function useRevealedVotes(addressOverride?: string) {
+  const queryClient = useQueryClient();
   const { voting } = useContractsContext();
   const { address: myAddress } = useAccountDetails();
   const { isWrongChain } = useWalletContext();
@@ -17,15 +18,21 @@ export function useRevealedVotes(addressOverride?: string) {
   const { onError } = useHandleError({ isDataFetching: true });
   const address = addressOverride ?? myAddress;
 
-  const queryResult = useQuery(
-    [revealedVotesKey, address, roundId],
-    () => getRevealedVotes(voting, address, roundId),
-    {
-      enabled: !!address && !isWrongChain,
-      initialData: {},
-      onError,
-    }
-  );
+  const queryKey = [revealedVotesKey, address, roundId];
+  const queryResult = useQuery({
+    queryKey,
+    // we update cache client side when we know we successfully revealed, so we want to merge data in the cache,
+    // and not override, in case the web3 call returns empty as we use a different provider to query vs write.
+    queryFn: () => getRevealedVotes(voting, address, roundId),
+    onSuccess(data) {
+      queryClient.setQueryData<typeof data>(queryKey, (oldRevealedVotes) =>
+        oldRevealedVotes ? { ...oldRevealedVotes, ...data } : data
+      );
+    },
+    enabled: !!address && !isWrongChain,
+    initialData: {},
+    onError,
+  });
 
   return queryResult;
 }
