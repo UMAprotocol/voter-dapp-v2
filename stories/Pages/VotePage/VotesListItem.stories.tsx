40b7889cd7c044fd03c3a6e3f87ccc0bbd720e1d
ryanwolhuter <dev@ryanwolhuter.com>
2023-03-27 11:39:16 +0200

    add custom input tests
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- stories/Pages/VotePage/VotesListItem.stories.tsx
+++ stories/Pages/VotePage/VotesListItem.stories.tsx
@@ -1,8 +1,12 @@
+import { expect } from "@storybook/jest";
 import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
 import { VotesList, VotesListItem, VotesTableHeadings } from "components";
 import { useState } from "react";
 import {
   polymarketVote,
+  polymarketVoteCommitted,
+  polymarketVoteCommittedCustomInput,
   voteCommitted,
   voteCommittedButNotRevealed,
   voteRevealed,
@@ -144,5 +148,72 @@ export const PolymarketNotCommitted: Story = {
     ...activeVotesCommonArgs,
     phase: "commit",
     vote: polymarketVote,
+  },
+};
+
+export const TestCustomInputNotCommitted: Story = {
+  ...PolymarketNotCommitted,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement.parentElement as HTMLElement);
+    let chooseAnswer = await canvas.findByText("Choose answer");
+    await waitFor(() => userEvent.click(chooseAnswer));
+    const custom = await canvas.findByText("Custom");
+    await waitFor(() => userEvent.click(custom));
+    await waitFor(() => userEvent.click(custom));
+    const input = await canvas.findByPlaceholderText("Enter value");
+    await waitFor(() => userEvent.type(input, "123"));
+    const exitButton = await canvas.findByLabelText("exit custom input");
+    await waitFor(() => userEvent.click(exitButton));
+    chooseAnswer = await canvas.findByText("Choose answer");
+    expect(chooseAnswer).toBeInTheDocument();
+  },
+};
+
+export const PolymarketCommitted: Story = {
+  ...Template,
+  args: {
+    ...activeVotesCommonArgs,
+    phase: "commit",
+    vote: polymarketVoteCommitted,
+  },
+};
+
+export const TestCustomInputCommitted: Story = {
+  ...PolymarketCommitted,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement.parentElement as HTMLElement);
+    let chosenAnswer = await canvas.findByText("Something");
+    await waitFor(() => userEvent.click(chosenAnswer));
+    const custom = await canvas.findByText("Custom");
+    await waitFor(() => userEvent.click(custom));
+    await waitFor(() => userEvent.click(custom));
+    const input = await canvas.findByPlaceholderText("Enter value");
+    await waitFor(() => userEvent.type(input, "123"));
+    const exitButton = await canvas.findByLabelText("exit custom input");
+    await waitFor(() => userEvent.click(exitButton));
+    chosenAnswer = await canvas.findByText("Something");
+    expect(chosenAnswer).toBeInTheDocument();
+  },
+};
+
+export const PolymarketCommittedCustomValue = {
+  ...Template,
+  args: {
+    ...activeVotesCommonArgs,
+    phase: "commit" as const,
+    vote: polymarketVoteCommittedCustomInput,
+  },
+};
+
+export const TestCustomInputCommittedWithCustomValue: Story = {
+  ...PolymarketCommittedCustomValue,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement.parentElement as HTMLElement);
+    const customValueInput = await canvas.findByDisplayValue("123");
+    expect(customValueInput).toBeInTheDocument();
+    const exitButton = await canvas.findByLabelText("exit custom input");
+    await waitFor(() => userEvent.click(exitButton));
+    const chooseAnswer = await canvas.findByText("Choose answer");
+    expect(chooseAnswer).toBeInTheDocument();
   },
 };
