34c10eab03b001fb19e4f8313c0554c821add03b
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-16 16:14:14 +0200

    return zero address when no address
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/delegation/getDelegateToStaker.ts
+++ web3/queries/delegation/getDelegateToStaker.ts
@@ -1,8 +1,10 @@
 import { VotingV2Ethers } from "@uma/contracts-frontend";
+import { zeroAddress } from "helpers";
 
 export function getDelegateToStaker(
   voting: VotingV2Ethers,
   delegateAddress: string
 ) {
+  if (!delegateAddress) return zeroAddress;
   return voting.delegateToStaker(delegateAddress);
 }
