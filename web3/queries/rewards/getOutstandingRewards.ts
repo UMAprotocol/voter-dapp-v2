860a07fcd0dd3b92c96bbb2745c57865e39a5237
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-16 16:06:58 +0200

    return zero when no address
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- web3/queries/rewards/getOutstandingRewards.ts
+++ web3/queries/rewards/getOutstandingRewards.ts
@@ -1,9 +1,11 @@
 import { VotingV2Ethers } from "@uma/contracts-frontend";
+import { BigNumber } from "ethers";
 
 export async function getOutstandingRewards(
   votingContract: VotingV2Ethers,
   address: string
 ) {
+  if (!address) return BigNumber.from(0);
   const result = await votingContract.functions.outstandingRewards(address);
   return result?.[0];
 }
