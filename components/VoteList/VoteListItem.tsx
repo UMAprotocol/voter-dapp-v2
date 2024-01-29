31210e324d548b850866cc4871d551e319cc36a5
Gerhard Steenkamp <gerhard@umaproject.org>
2024-01-29 18:39:05 +0200

    remove manual split

--- components/VoteList/VoteListItem.tsx
+++ components/VoteList/VoteListItem.tsx
@@ -51,7 +51,7 @@ export function VoteListItem(props: VoteListItemProps) {
     useOptimisticGovernorData(props.vote.decodedAncillaryData);
 
   const optimisticGovernorTitle = isOptimisticGovernorVote
-    ? getOptimisticGovernorTitle(explanationText).slice(0, 50) + "..."
+    ? getOptimisticGovernorTitle(explanationText)
     : "";
 
   const voteOrigin = isOptimisticGovernorVote ? "OSnap" : origin;
