510d03fabbd568cf6b95d0eea4b9f407b4ceb80c
david <david@umaproject.org>
2024-05-14 07:38:43 -0400

    feat: add multiple choice query parsing

--- constant/voting/earlyRequestMagicNumber.ts
+++ constant/voting/earlyRequestMagicNumber.ts
@@ -3,6 +3,11 @@ export const earlyRequestMagicNumber =
 export const earlyRequestMagicNumberWei =
   "-57896044618658097711785492504343953926634992332820282019728792003956564819968";
 
+export const answerNotPossible =
+  "57896044618658097711785492504343953926634992332820282019728.792003956564819967";
+export const answerNotPossibleWei =
+  "57896044618658097711785492504343953926634992332820282019728792003956564819967";
+
 export function isEarlyVote(value: string | undefined):boolean{
   if(value === undefined) return false
   return [earlyRequestMagicNumber, earlyRequestMagicNumberWei].includes(value)
