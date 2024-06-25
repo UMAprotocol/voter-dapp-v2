8680dc279c150e09f5ab7eac541e39fb9c6571c8
David A <4429761+daywiss@users.noreply.github.com>
2024-06-25 06:04:10 -0400

    fix: reduce log lookbacks to fix reveal (#313)
    Signed-off-by: david <david@umaproject.org>

--- web3/queries/votes/getRevealedVotes.ts
+++ web3/queries/votes/getRevealedVotes.ts
@@ -18,7 +18,11 @@ export async function getRevealedVotes(
     null,
     null
   );
-  const result = await votingContract.queryFilter(filter);
+  const currentBlock = await votingContract.provider.getBlockNumber();
+  const result = await votingContract.queryFilter(
+    filter,
+    currentBlock - 100000
+  );
   const eventData = result.filter(({ args }) => args.roundId === roundId);
   const revealedVotes: VoteExistsByKeyT = {};
 
