617673c8df1a5d4a849edd2b3258b0ae05788588
david <david@umaproject.org>
2024-08-02 16:05:23 -0400

    fix: look for past votes further back
    Signed-off-by: david <david@umaproject.org>

--- web3/queries/votes/getUpcomingVotes.ts
+++ web3/queries/votes/getUpcomingVotes.ts
@@ -7,7 +7,8 @@ export async function getUpcomingVotes(
 ) {
   const currentBlock = await voting.provider.getBlockNumber();
   const filter = voting.filters.RequestAdded(null, null, null);
-  const result = await voting.queryFilter(filter, currentBlock - 10000);
+  // this needs to look back at least 48 hours for a full voting cycle, lets do 2 full cycles just in case this is roughly 30k blocks
+  const result = await voting.queryFilter(filter, currentBlock - 30000);
   const eventData = result?.map(({ args }) => args);
   const onlyUpcoming = eventData?.filter((event) => event.roundId > roundId);
   const upcomingVotes = makePriceRequestsByKey(onlyUpcoming);
