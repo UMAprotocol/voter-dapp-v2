2ff6a02b4494a99472fdec4235f3dda7d746c7db
Gerhard Steenkamp <gerhard@umaproject.org>
2025-02-07 11:54:52 +0200

    add vote history banner

--- components/AnnouncementBanners/VoteHistoryBanner.tsx
+++ components/AnnouncementBanners/VoteHistoryBanner.tsx
@@ -1,23 +1,16 @@
 import { mobileAndUnder } from "constant";
-import NextLink from "next/link";
 import Info from "public/assets/icons/information.svg";
 import styled from "styled-components";
 import { AnnouncementBannerWrapper } from "./AnnouncementBannerWrapper";
 
-export function GasRebateBanner() {
+export function VoteHistoryBanner() {
   return (
-    <AnnouncementBannerWrapper localStorageKey="show-gas-rebate-banner">
+    <AnnouncementBannerWrapper localStorageKey="show-vote-history-banner">
       <InfoIcon />
       <Text>
-        New voting gas rebate rules come into effect starting February 1st, see
-        details{" "}
-        <Link
-          href="https://docs.uma.xyz/using-uma/voting-gas-rebates"
-          target="_blank"
-        >
-          here
-        </Link>
-        .
+        <strong>Voter history</strong> and interim <strong>vote results</strong>{" "}
+        during the reveal phase are temporarily not displaying correctly due to
+        data provider errors from increased activity.
       </Text>
     </AnnouncementBannerWrapper>
   );
@@ -39,12 +32,4 @@ const Text = styled.p`
   font: var(--text-md);
   font-size: clamp(0.75rem, calc(0.45rem + 1.6vw), 1rem);
   color: var(--white);
-`;
-
-const Link = styled(NextLink)`
-  color: var(--red-500);
-  text-decoration: none;
-  &:hover {
-    text-decoration: underline;
-  }
 `;
