61eca1b852474c9ae76c97ff59984307e59d79ea
Gerhard Steenkamp <51655063+gsteenkamp89@users.noreply.github.com>
2025-06-26 12:25:34 +0200

    sanitize titles on the frontend also (#378)
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Strip invalid characters that Discord strips from titles
+export function stripInvalidCharacters(str: string): string {
+  const invalidCharacters = [
+    "\u202f", // hyphenation point
+  ];
+  let sanitized = str;
+  for (const char of invalidCharacters) {
+    sanitized = sanitized.replace(new RegExp(char, "g"), "");
+  }
+  return sanitized;
+}
