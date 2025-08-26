5b0dcafdba15dc6a17e6a5f796c029a41d3fbe53
Gerhard Steenkamp <gerhard@umaproject.org>
2025-08-26 19:04:29 +0200

    fixup
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- helpers/voting/projects/polymarket.ts
+++ helpers/voting/projects/polymarket.ts
@@ -17,13 +17,12 @@ export const polymarketRequesters = [
   "0xb21182d0494521cf45dbbeebb5a3acaab6d22093", // Polymarket Sports Address
   "0x157ce2d672854c848c9b79c49a8cc6cc89176a49", // Polymarket CTF Adapter Address V3
   "0x65070be91477460d8a7aeeb94ef92fe056c2f2a7", // Polymarket UmaCtfAdapter for Managed OOv2
-];
+].map((a) => a.toLowerCase());
+
 const polymarketIdentifiers = ["YES_OR_NO_QUERY", "MULTIPLE_VALUES"];
 
 export function isPolymarketRequester(address: string): boolean {
-  return polymarketRequesters
-    .map((a) => a.toLowerCase())
-    .includes(address.toLowerCase());
+  return polymarketRequesters.includes(address.toLowerCase());
 }
 
 export function getRequester(decodedAncillaryData: string): string | undefined {
