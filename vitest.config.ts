6b3d60d29c277ae749e58d3d5e1f7b55b27244af
Gerhard Steenkamp <gerhard@umaproject.org>
2025-09-19 23:13:57 +0200

    use node env for tests for now
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: "jsdom",
+    environment: "node",
     globals: true,
   },
 });
