ea78c03ba6bed421c194801d3f9d4622ab0ad6d9
Gerhard Steenkamp <51655063+gsteenkamp89@users.noreply.github.com>
2026-01-19 22:25:04 +0200

    handle null ancil data when making key (#428)

--- helpers/voting/makeUniqueKeyForVote.ts
+++ helpers/voting/makeUniqueKeyForVote.ts
@@ -3,11 +3,13 @@ import { BigNumber, utils } from "ethers";
 export function makeUniqueKeyForVote(
   decodedIdentifier: string,
   time: number | BigNumber,
-  ancillaryData: string
+  ancillaryData: string | null | undefined
 ) {
   if (typeof time !== "number") {
     time = time.toNumber();
   }
-  const ancillaryDataHash = utils.keccak256(utils.toUtf8Bytes(ancillaryData));
+  const ancillaryDataHash = utils.keccak256(
+    utils.toUtf8Bytes(ancillaryData ?? "")
+  );
   return `${decodedIdentifier}-${time}-${ancillaryDataHash}`;
 }
