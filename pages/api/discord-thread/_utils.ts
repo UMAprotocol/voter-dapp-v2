89063111be466646ef8cd49f97a6fb7fe0e74a64
Jørgen <jorgen@umaproject.org>
2026-03-18 10:03:47 +0100

    use prod redis cache (temporarily - revert before merge)

--- pages/api/discord-thread/_utils.ts
+++ pages/api/discord-thread/_utils.ts
@@ -59,7 +59,14 @@ export function getRedis(): Redis | null {
   if (DISABLE_REDIS) {
     return null;
   }
-  redis ??= Redis.fromEnv();
+  redis ??= new Redis({
+    url:
+      process.env.KV_REST_API_URL ?? process.env.UPSTASH_REDIS_REST_URL ?? "",
+    token:
+      process.env.KV_REST_API_TOKEN ??
+      process.env.UPSTASH_REDIS_REST_TOKEN ??
+      "",
+  });
   return redis;
 }
 
